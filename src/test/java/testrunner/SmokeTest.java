1adfed9d1e1fa1732b62fb9c0d0b7cbb40fd610c
philip_sushkov <philips@q4websystems.com>
2016-11-07 09:35:00 -0500

    EnterToAdmin.java added

--- src/test/java/testrunner/SmokeTest.java
+++ src/test/java/testrunner/SmokeTest.java
@@ -2,10 +2,15 @@ package testrunner;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import specs.LoginPage.EnterToAdmin;
+
+/**
+ * Created by philips on 2016-11-07.
+ */
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-
+        EnterToAdmin.class
 })
 
 public class SmokeTest {
