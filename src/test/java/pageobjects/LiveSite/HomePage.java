7e966c9a3b18ca162da6d4cc59052cc92b1cf42f
KelTez <macabessa123@gmail.com>
2016-11-21 11:01:36 -0500

    Added incomplete emailAlertsPage tests
    These are incomplete as random timeout failures occur, so I cannot test
    for the correct logic as of now. Close to a resolution, however.

--- src/test/java/pageobjects/LiveSite/HomePage.java
+++ src/test/java/pageobjects/LiveSite/HomePage.java
@@ -1,6 +1,8 @@
 package pageobjects.LiveSite;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import pageobjects.AbstractPageObject;
 
@@ -20,7 +22,14 @@ public class HomePage extends AbstractPageObject {
     private final By boardOfDirectors = By.linkText("Board of Directors");
     private final By rssFeeds = By.linkText("RSS Feeds");
     private final By siteMap = By.linkText("Site Map");
+<<<<<<< Updated upstream
     private final By emailAlerts = By.linkText("Email Alerts");
+=======
+    private final By emailAlerts = By.linkText("Email Alerts"); //By.id("_ctrl0_ctl45_SiteMapRepeater_ctl15_hrefLink"); //Exists/isDislpayed once siteMap is clicked
+
+    // ^^ Currently the above is within a <ul></ul> which is an unordered list. Perhaps attempt to get the X element of that list with x-path to get the emailAlerts as it is not working as of now
+
+>>>>>>> Stashed changes
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -29,6 +38,10 @@ public class HomePage extends AbstractPageObject {
     public boolean logoIsPresent(){
 
         return doesElementExist(Q4Logo);
+    }
+
+    public void clickSiteMap(){
+        findElement(siteMap).click();
     }
 
     public String getVersionNumber(){
@@ -76,10 +89,30 @@ public class HomePage extends AbstractPageObject {
         return new RSSFeedsPage(getDriver());
     }
 
+<<<<<<< Updated upstream
     public RSSFeedsPage emailAlertsFromMap(){
         findVisibleElement(siteMap).click();
         pause(1000L);
         findVisibleElement(emailAlerts).click();
         return new RSSFeedsPage(getDriver());
     }
+=======
+    public EmailAlertsPage selectEmailAlertsFromPage() { //perhaps just go to URL instead of clicking on elements?
+    /*
+        findVisibleElement(siteMap).click();
+        pause(500L);
+        driver.findElement(By.tagName("body")).sendKeys(Keys.ESCAPE);
+        wait.until(ExpectedConditions.visibilityOf(findElement(emailAlerts)));
+        findElement(emailAlerts).click();
+*/
+// /*
+        driver.get("http://chicagotest.q4web.com/English/Contact-Us/email-alerts/default.aspx");
+        driver.findElement(By.tagName("body")).sendKeys("Keys.ESCAPE");
+        driver.findElement(By.tagName("body")).sendKeys(Keys.ESCAPE);
+        ((JavascriptExecutor)driver).executeScript("return window.stop");
+//*/
+        return new EmailAlertsPage(getDriver());
+    }
+
+>>>>>>> Stashed changes
 }
