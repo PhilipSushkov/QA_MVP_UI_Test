6f26e7e7db2f27306688732561af938576ad0768
philip_sushkov <philips@q4websystems.com>
2016-12-20 13:33:19 -0500

    CheckLayoutDefinitionEdit, CheckModuleDefinitionEdit added

--- src/test/java/pageobjects/SiteAdmin/LayoutDefinitionList/LayoutDefinitionEdit.java
+++ src/test/java/pageobjects/SiteAdmin/LayoutDefinitionList/LayoutDefinitionEdit.java
@@ -1,4 +1,4 @@
-package pageobjects.SiteAdmin.GlobalModuleList;
+package pageobjects.SiteAdmin.LayoutDefinitionList;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementNotVisibleException;
@@ -12,16 +12,16 @@ import static specs.AbstractSpec.propUISiteAdmin;
  * Created by philipsushkov on 2016-12-20.
  */
 
-public class GlobalModuleEdit extends AbstractPageObject {
-    private static By moduleTitle, moduleTitleField, moduleDefinitionSelect, moduleTypeSelect, regionNameSelect, saveAndSubmitButton;
+public class LayoutDefinitionEdit extends AbstractPageObject {
+    private static By moduleTitle, friendlyNameField, descriptionField, defaultLayoutSelect, activeCheckbox, saveAndSubmitButton;
 
-    public GlobalModuleEdit(WebDriver driver) {
+    public LayoutDefinitionEdit(WebDriver driver) {
         super(driver);
-        moduleTitle = By.xpath(propUISiteAdmin.getProperty("spanSection_Title"));
-        moduleTitleField = By.xpath(propUISiteAdmin.getProperty("input_ModuleTitle"));
-        moduleDefinitionSelect = By.xpath(propUISiteAdmin.getProperty("select_ModuleDefinition"));
-        moduleTypeSelect = By.xpath(propUISiteAdmin.getProperty("select_ModuleType"));
-        regionNameSelect = By.xpath(propUISiteAdmin.getProperty("select_RegionName"));
+        moduleTitle = By.xpath(propUISiteAdmin.getProperty("spanModule_Title"));
+        friendlyNameField = By.xpath(propUISiteAdmin.getProperty("input_FriendlyName"));
+        descriptionField = By.xpath(propUISiteAdmin.getProperty("input_Description"));
+        defaultLayoutSelect = By.xpath(propUISiteAdmin.getProperty("select_DefaultLayout"));
+        activeCheckbox = By.xpath(propUISiteAdmin.getProperty("chk_Active"));
         saveAndSubmitButton = By.xpath(propUISiteAdmin.getProperty("btn_SaveAndSubmit"));
     }
 
@@ -30,12 +30,12 @@ public class GlobalModuleEdit extends AbstractPageObject {
         return getText(moduleTitle);
     }
 
-    public WebElement getModuleTitleInput() {
+    public WebElement getFriendlyNameInput() {
         WebElement element = null;
 
         try {
-            waitForElement(moduleTitleField);
-            element = findElement(moduleTitleField);
+            waitForElement(friendlyNameField);
+            element = findElement(friendlyNameField);
         } catch (ElementNotFoundException e1) {
         } catch (ElementNotVisibleException e2) {
         }
@@ -43,12 +43,12 @@ public class GlobalModuleEdit extends AbstractPageObject {
         return element;
     }
 
-    public WebElement getModuleDefinitionSelect() {
+    public WebElement getDescriptionInput() {
         WebElement element = null;
 
         try {
-            waitForElement(moduleDefinitionSelect);
-            element = findElement(moduleDefinitionSelect);
+            waitForElement(descriptionField);
+            element = findElement(descriptionField);
         } catch (ElementNotFoundException e1) {
         } catch (ElementNotVisibleException e2) {
         }
@@ -56,12 +56,12 @@ public class GlobalModuleEdit extends AbstractPageObject {
         return element;
     }
 
-    public WebElement getModuleTypeSelect() {
+    public WebElement getDefaultLayoutSelect() {
         WebElement element = null;
 
         try {
-            waitForElement(moduleTypeSelect);
-            element = findElement(moduleTypeSelect);
+            waitForElement(defaultLayoutSelect);
+            element = findElement(defaultLayoutSelect);
         } catch (ElementNotFoundException e1) {
         } catch (ElementNotVisibleException e2) {
         }
@@ -69,12 +69,12 @@ public class GlobalModuleEdit extends AbstractPageObject {
         return element;
     }
 
-    public WebElement getRegionNameSelect() {
+    public WebElement getActiveCheckbox() {
         WebElement element = null;
 
         try {
-            waitForElement(regionNameSelect);
-            element = findElement(regionNameSelect);
+            waitForElement(activeCheckbox);
+            element = findElement(activeCheckbox);
         } catch (ElementNotFoundException e1) {
         } catch (ElementNotVisibleException e2) {
         }
---
+++ src/test/java/specs/SiteAdmin/ModuleDefinitionList/CheckModuleDefinitionEdit.java
@@ -0,0 +1,61 @@
+package specs.SiteAdmin.ModuleDefinitionList;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+import org.openqa.selenium.By;
+import pageobjects.SiteAdmin.ModuleDefinitionList.ModuleDefinitionEdit;
+import specs.AbstractSpec;
+import pageobjects.LoginPage.LoginPage;
+import pageobjects.Dashboard.Dashboard;
+
+/**
+ * Created by philipsushkov on 2016-12-20.
+ */
+
+public class CheckModuleDefinitionEdit extends AbstractSpec {
+
+    private static By siteAdminMenuButton, moduleDefinitionListMenuItem, userAddNewLink;
+    private static LoginPage loginPage;
+    private static Dashboard dashboard;
+    private static ModuleDefinitionEdit moduleDefinitionEdit;
+
+    @BeforeTest
+    public void setUp() throws Exception {
+        siteAdminMenuButton = By.xpath(propUISiteAdmin.getProperty("btnMenu_SiteAdmin"));
+        moduleDefinitionListMenuItem = By.xpath(propUISiteAdmin.getProperty("itemMenu_ModuleDefinitionList"));
+        userAddNewLink = By.xpath(propUISystemAdmin.getProperty("input_AddNew"));
+
+        loginPage = new LoginPage(driver);
+        dashboard = new Dashboard(driver);
+        moduleDefinitionEdit = new ModuleDefinitionEdit(driver);
+
+        loginPage.loginUser();
+        dashboard.openEditPageFromAddNew(siteAdminMenuButton, moduleDefinitionListMenuItem, userAddNewLink);
+    }
+
+    @Test
+    public void checkModuleDefinitionEdit() throws Exception {
+        final String expectedTitle = "Module Definition Edit";
+
+        Assert.assertNotNull(moduleDefinitionEdit.getUrl());
+        Assert.assertEquals(moduleDefinitionEdit.getTitle(), expectedTitle, "Actual Module Definition Edit page Title doesn't match to expected");
+
+        Assert.assertNotNull(moduleDefinitionEdit.getFriendlyNameInput(), "Friendly Name field doesn't exist");
+        Assert.assertNotNull(moduleDefinitionEdit.getQualifiedPathInput(), "Qualified Path Input doesn't exist");
+        Assert.assertNotNull(moduleDefinitionEdit.getLinkToEditPageSelect(), "Link To Edit Page Select doesn't exist");
+        Assert.assertNotNull(moduleDefinitionEdit.getLinkToListPageSelect(), "Link To List Page Select doesn't exist");
+        Assert.assertNotNull(moduleDefinitionEdit.getLinkToAdminPageSelect(), "Link To Admin Page Select doesn't exist");
+        Assert.assertNotNull(moduleDefinitionEdit.getActiveCheckbox(), "Active Checkbox doesn't exist");
+        Assert.assertNotNull(moduleDefinitionEdit.getSaveAndSubmitButton(), "Save And Submit Button doesn't exist");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        dashboard.logoutFromAdmin();
+        //driver.quit();
+    }
+
+}
