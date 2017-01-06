98555d4caf1d42fa51e5c40ab2c677fcfe661b3f
philip_sushkov <philips@q4websystems.com>
2017-01-06 13:55:05 -0500

    CheckRegionEdit added

--- src/test/java/pageobjects/ContentAdmin/Region/RegionEdit.java
+++ src/test/java/pageobjects/ContentAdmin/Region/RegionEdit.java
@@ -1,7 +1,126 @@
 package pageobjects.ContentAdmin.Region;
 
+import org.openqa.selenium.*;
+import pageobjects.AbstractPageObject;
+
+import static specs.AbstractSpec.propUIContentAdmin;
+
 /**
  * Created by philipsushkov on 2017-01-06.
  */
-public class RegionEdit {
+
+public class RegionEdit extends AbstractPageObject {
+
+    private static By moduleTitle, regionNameInput, globalOperationSelect, saveAndSubmitButton;
+    private static By tagsInput, bodyFrame, hideReportsCheckbox, activeCheckbox;
+
+    public RegionEdit(WebDriver driver) {
+        super(driver);
+        moduleTitle = By.xpath(propUIContentAdmin.getProperty("span_Title"));
+
+        regionNameInput = By.xpath(propUIContentAdmin.getProperty("input_RegionName"));
+        globalOperationSelect = By.xpath(propUIContentAdmin.getProperty("select_GlobalOperation"));
+        tagsInput = By.xpath(propUIContentAdmin.getProperty("input_Tags"));
+        bodyFrame = By.xpath(propUIContentAdmin.getProperty("frame_Body"));
+
+        hideReportsCheckbox = By.xpath(propUIContentAdmin.getProperty("chk_HideReports"));
+        activeCheckbox = By.xpath(propUIContentAdmin.getProperty("chk_Active"));
+
+        saveAndSubmitButton = By.xpath(propUIContentAdmin.getProperty("btn_SaveAndSubmit"));
+    }
+
+    public String getTitle() {
+        waitForElement(moduleTitle);
+        return getText(moduleTitle);
+    }
+
+    public WebElement getRegionNameInput() {
+        WebElement element = null;
+
+        try {
+            waitForElement(regionNameInput);
+            element = findElement(regionNameInput);
+        } catch (ElementNotFoundException e1) {
+        } catch (ElementNotVisibleException e2) {
+        } catch (TimeoutException e3) {
+        }
+
+        return element;
+    }
+
+    public WebElement getGlobalOperationSelect() {
+        WebElement element = null;
+
+        try {
+            waitForElement(globalOperationSelect);
+            element = findElement(globalOperationSelect);
+        } catch (ElementNotFoundException e1) {
+        } catch (ElementNotVisibleException e2) {
+        } catch (TimeoutException e3) {
+        }
+
+        return element;
+    }
+
+    public WebElement getTagsInput() {
+        WebElement element = null;
+
+        try {
+            waitForElement(tagsInput);
+            element = findElement(tagsInput);
+        } catch (ElementNotFoundException e1) {
+        } catch (ElementNotVisibleException e2) {
+        } catch (TimeoutException e3) {
+        }
+
+        return element;
+    }
+
+    public WebElement getBodyFrame() {
+        WebElement element = null;
+
+        try {
+            waitForElement(bodyFrame);
+            element = findElement(bodyFrame);
+        } catch (ElementNotFoundException e1) {
+        } catch (ElementNotVisibleException e2) {
+        } catch (TimeoutException e3) {
+        }
+
+        return element;
+    }
+
+    public Boolean getChkBoxSet() {
+        Boolean chkBoxSet = false;
+
+        try {
+            waitForElement(hideReportsCheckbox);
+            findElement(hideReportsCheckbox);
+
+            waitForElement(activeCheckbox);
+            findElement(activeCheckbox);
+
+            chkBoxSet = true;
+        } catch (ElementNotFoundException e1) {
+        } catch (ElementNotVisibleException e2) {
+        } catch (TimeoutException e3) {
+        }
+
+        return chkBoxSet;
+    }
+
+    public WebElement getSaveAndSubmitButton() {
+        WebElement element = null;
+
+        try {
+            waitForElement(saveAndSubmitButton);
+            element = findElement(saveAndSubmitButton);
+        } catch (ElementNotFoundException e1) {
+        } catch (ElementNotVisibleException e2) {
+        } catch (TimeoutException e3) {
+        }
+
+        return element;
+    }
+
 }
--- src/test/java/specs/ContentAdmin/Region/CheckRegionEdit.java
+++ src/test/java/specs/ContentAdmin/Region/CheckRegionEdit.java
@@ -1,7 +1,62 @@
 package specs.ContentAdmin.Region;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+import org.openqa.selenium.By;
+import pageobjects.ContentAdmin.Region.RegionEdit;
+import specs.AbstractSpec;
+import pageobjects.LoginPage.LoginPage;
+import pageobjects.Dashboard.Dashboard;
+
 /**
  * Created by philipsushkov on 2017-01-06.
  */
-public class CheckRegionEdit {
+
+public class CheckRegionEdit extends AbstractSpec {
+
+    private static By contentAdminMenuButton, regionListMenuItem, addNewLink;
+    private static LoginPage loginPage;
+    private static Dashboard dashboard;
+    private static RegionEdit regionEdit;
+
+    @BeforeTest
+    public void setUp() throws Exception {
+        contentAdminMenuButton = By.xpath(propUIContentAdmin.getProperty("btnMenu_ContentAdmin"));
+        regionListMenuItem = By.xpath(propUIContentAdmin.getProperty("btnMenu_RegionList"));
+        addNewLink = By.xpath(propUIContentAdmin.getProperty("input_AddNew"));
+
+        loginPage = new LoginPage(driver);
+        dashboard = new Dashboard(driver);
+        regionEdit = new RegionEdit(driver);
+
+        loginPage.loginUser();
+        dashboard.openEditPageFromAddNew(contentAdminMenuButton, regionListMenuItem, addNewLink);
+    }
+
+    @Test
+    public void checkRegionEdit() throws Exception {
+        final String expectedTitle = "Region Edit";
+
+        Assert.assertNotNull(regionEdit.getUrl());
+        Assert.assertEquals(regionEdit.getTitle(), expectedTitle, "Actual Region Edit page Title doesn't match to expected");
+
+        Assert.assertNotNull(regionEdit.getRegionNameInput(), "Region Name field doesn't exist");
+        Assert.assertNotNull(regionEdit.getGlobalOperationSelect(), "Global Operation select doesn't exist");
+        Assert.assertNotNull(regionEdit.getTagsInput(), "Tags field doesn't exist");
+        Assert.assertNotNull(regionEdit.getBodyFrame(), "Body frame doesn't exist");
+
+        Assert.assertTrue(regionEdit.getChkBoxSet(), "Checkbox elements don't exist");
+
+        Assert.assertNotNull(regionEdit.getSaveAndSubmitButton(), "Save And Submit button doesn't exist");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        dashboard.logoutFromAdmin();
+        //driver.quit();
+    }
+
 }
