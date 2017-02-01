41962b50c0daee378d17705ba522527803b1df65
philip_sushkov <philips@q4websystems.com>
2017-02-01 10:16:06 -0500

    Code refactoring - 2

--- src/test/java/pageobjects/PageAdmin/PageAdminList.java
+++ src/test/java/pageobjects/PageAdmin/PageAdminList.java
@@ -29,6 +29,7 @@ public class PageAdminList extends AbstractPageObject {
     private static By moduleInstancesSpan, sectionTitleSpan, pageNameLbl, moduleNameLbl, editModuleImg, moduleDefinitionSelect;
     private static By workflowStateSpan;
     private static String sPathToFile, sDataFilePagesJson, sDataFileModulesJson;
+    private static JSONParser parser;
     private static final long DEFAULT_PAUSE = 1500;
 
     public PageAdminList(WebDriver driver) {
@@ -64,6 +65,8 @@ public class PageAdminList extends AbstractPageObject {
         sDataFilePagesJson = propUIPageAdmin.getProperty("json_PagesProp");
         sDataFileModulesJson = propUIPageAdmin.getProperty("json_ModulesProp");
         sPathToFile = System.getProperty("user.dir") + propUIPageAdmin.getProperty("dataPath_PageAdmin");
+
+        parser = new JSONParser();
     }
 
     public String getTitle() {
@@ -114,18 +117,13 @@ public class PageAdminList extends AbstractPageObject {
         boolean pageNames = true;
         By innerWrapPage;
         String[] sa2;
-        int i, j;
-        String itemID=null, sectionTitle, itemIDModule=null, you_page_url;
 
         sa2 = Functions.ReadArrayFromJSON(sPathToFile + sDataFilePagesJson, "page_names");
-
-        //Functions.ClearJSONfile(sPathToFile + sDataFilePagesJson);
         Functions.ClearJSONfile(sPathToFile + sDataFileModulesJson);
 
         try {
-            for (i=0; i<sa2.length; i++) {
-                //System.out.println(sa1[i]);
-                innerWrapPage = By.xpath("//div[contains(@id, 'divContent')]//span[contains(@class, 'innerWrap')][(text()=\""+sa2[i]+"\")]/parent::span/parent::a");
+            for (int pageSection=0; pageSection<sa2.length; pageSection++) {
+                innerWrapPage = By.xpath("//div[contains(@id, 'divContent')]//span[contains(@class, 'innerWrap')][(text()=\""+sa2[pageSection]+"\")]/parent::span/parent::a");
                 waitForElement(innerWrapPage);
                 findElement(innerWrapPage).click();
 
@@ -140,162 +138,9 @@ public class PageAdminList extends AbstractPageObject {
                     waitForElement(backBtn);
                     Thread.sleep(DEFAULT_PAUSE);
 
-                    // Create JSON object for Pages
-                    JSONParser parser = new JSONParser();
-                    JSONObject jsonObject = new JSONObject();
-                    JSONObject mmjson = new JSONObject();
-
-                    // Create JSON object for Modules
-                    JSONObject jsonObjectModule = new JSONObject();
-
-                    // -------------------------------------------------------------
-
-                    // Add values to JSON file
-                    try {
-                        try {
-                            jsonObject = (JSONObject) parser.parse(new FileReader(sPathToFile + sDataFilePagesJson));
-                            jsonObjectModule = (JSONObject) parser.parse(new FileReader(sPathToFile + sDataFileModulesJson));
-                        } catch (ParseException e) {
-                        }
-
-                        URL pageURL = new URL(getUrl());
-
-                        String[] params = pageURL.getQuery().split("&");
-                        JSONObject jsonURLQuery = new JSONObject();
-
-                        for (String param : params) {
-                            jsonURLQuery.put(param.split("=")[0], param.split("=")[1]);
-
-                            if (param.split("=")[0].equals("ItemID")) {
-                                itemID = param.split("=")[1];
-                            }
-                        }
-
-                        mmjson.put("url_query", jsonURLQuery);
-
-                        sectionTitle = findElement(sectionTitleInput).getAttribute("value");
-                        mmjson.put("section_title", sectionTitle);
-                        you_page_url = findElement(yourPageUrlLabel).getText() + findElement(seoNameInput).getAttribute("value");
-                        mmjson.put("you_page_url", you_page_url);
-                        mmjson.put("page_title", findElement(pageTitleInput).getAttribute("value"));
-
-                        if (Boolean.parseBoolean(findElement(pageTypeInternalRd).getAttribute("checked"))) {
-                            mmjson.put("page_type", "Internal");
-                            mmjson.put("page_layout", new Select(driver.findElement(pageLayoutSelect)).getFirstSelectedOption().getText());
-                            mmjson.put("description", findElement(descriptionTextarea).getText());
-                            mmjson.put("domain", new Select(driver.findElement(domainSelect)).getFirstSelectedOption().getText());
-
-                            findElement(switchBtn).click();
-                            Thread.sleep(DEFAULT_PAUSE);
-
-                        } else if (Boolean.parseBoolean(findElement(pageTypeExternalRd).getAttribute("checked"))) {
-                            mmjson.put("page_type", "External");
-                            mmjson.put("external_url", findElement(externalURLInput).getAttribute("value"));
-                        }
-
-                        mmjson.put("active", findElement(activeChk).getAttribute("checked"));
-                        mmjson.put("workflow_state", findElement(workflowStateSpan).getText());
-
-                        // Save Global Module Settings list
-                        List<WebElement> globalModuleInputs;
-                        List<WebElement> globalModuleLabels;
-                        JSONArray globalModuleSettings = new JSONArray();
-                        j = 0;
-                        globalModuleInputs = findElements(globalModuleSetChk);
-                        globalModuleLabels = findElements(globalModuleSetLbl);
-                        for (WebElement globalModuleInput : globalModuleInputs) {
-                            if (Boolean.parseBoolean(globalModuleInput.getAttribute("checked"))) {
-                                globalModuleSettings.add(globalModuleLabels.get(j).getText());
-                            }
-                            j++;
-                        }
-                        mmjson.put("global_module_settings", globalModuleSettings);
-
-                        // Save Modules list
-                        List<WebElement> moduleSpans;
-                        JSONArray moduleList = new JSONArray();
-                        j = 0;
-                        moduleSpans = findElements(moduleInstancesSpan);
-                        for (WebElement moduleSpan : moduleSpans) {
-                            moduleList.add(moduleSpan.getText());
-                            j++;
-                        }
-                        mmjson.put("modules", moduleList);
-
-                        // ----------------------------------------------------
-                        // Open Module pages
-                        List<WebElement> modulePages;
-                        modulePages = findElements(moduleInstancesSpan);
-
-                        for (int moduleNum=0; moduleNum<modulePages.size(); moduleNum++) {
-                            findElements(editModuleImg).get(moduleNum).click();
-                            waitForElement(backBtn);
-
-                            JSONObject pageParameters = new JSONObject();
-                            pageParameters.put("section_title", sectionTitle);
-                            pageParameters.put("page_url", you_page_url);
-
-                            JSONObject mmjsonModule = new JSONObject();
-                            mmjsonModule.put(itemID, pageParameters);
-
-                            URL pageURLModule = new URL(getUrl());
-
-                            String[] paramModules = pageURLModule.getQuery().split("&");
-                            JSONObject jsonURLQueryModule = new JSONObject();
-
-                            for (String paramparamModule : paramModules) {
-                                jsonURLQueryModule.put(paramparamModule.split("=")[0], paramparamModule.split("=")[1]);
-
-                                if (paramparamModule.split("=")[0].equals("ItemID")) {
-                                    itemIDModule = paramparamModule.split("=")[1];
-                                }
-                            }
-
-                            mmjsonModule.put("url_query", jsonURLQueryModule);
-                            mmjsonModule.put(new Select(driver.findElement(moduleDefinitionSelect)).getFirstSelectedOption().getText(), you_page_url);
-                            mmjsonModule.put("active", findElement(activeChk).getAttribute("checked"));
-                            mmjsonModule.put("workflow_state", findElement(workflowStateSpan).getText());
-
-                            jsonObjectModule.put(itemIDModule, mmjsonModule);
-
-                            findElement(backBtn).click();
-                        }
-                        // ----------------------------------------------------
-
-                        // Save Sub Pages list
-                        List<WebElement> subPagesSpans;
-                        JSONArray subPagesList = new JSONArray();
-                        j = 0;
-                        subPagesSpans = findElements(sectionTitleSpan);
-                        for (WebElement subPagesSpan : subPagesSpans) {
-                            subPagesList.add(subPagesSpan.getText());
-                            j++;
-                        }
-                        mmjson.put("subpages", subPagesList);
-                        jsonObject.put(sectionTitle, mmjson);
-
-                        FileWriter file = new FileWriter(sPathToFile + sDataFilePagesJson);
-                        file.write(jsonObject.toJSONString().replace("\\", ""));
-                        file.flush();
-                        file.close();
-
-                        FileWriter fileModule = new FileWriter(sPathToFile + sDataFileModulesJson);
-                        fileModule.write(jsonObjectModule.toJSONString().replace("\\", ""));
-                        fileModule.flush();
-                        fileModule.close();
-
-                    } catch (FileNotFoundException e1) {
-                        e1.printStackTrace();
-                        pageNames = false;
-                    } catch (IOException e2) {
-                        e2.printStackTrace();
-                        pageNames = false;
-                    }
-
-                    // -------------------------------------------------------------
+                    savePageToJSON(sPathToFile, sDataFilePagesJson, sDataFileModulesJson);
 
                     findElement(backBtn).click();
-
                     waitForElement(dataGridItemBorder);
                 }
             }
@@ -339,8 +184,12 @@ public class PageAdminList extends AbstractPageObject {
         return urlPage;
     }
 
-    public JSONObject savePagetoJSON () {
+    public JSONObject savePageToJSON (String sPathToFile, String sDataFilePagesJson, String sDataFileModulesJson) throws InterruptedException {
+        String itemID=null, sectionTitle, itemIDModule=null, you_page_url;
+        int j;
         JSONObject jsonObject = new JSONObject();
+        JSONObject jsonObjectModule = new JSONObject();
+        JSONObject mmjson = new JSONObject();
 
         // Add values to JSON file
         try {
@@ -451,7 +300,13 @@ public class PageAdminList extends AbstractPageObject {
                 jsonObjectModule.put(itemIDModule, mmjsonModule);
 
                 findElement(backBtn).click();
+
+                FileWriter fileModule = new FileWriter(sPathToFile + sDataFileModulesJson);
+                fileModule.write(jsonObjectModule.toJSONString().replace("\\", ""));
+                fileModule.flush();
+                fileModule.close();
             }
+
             // ----------------------------------------------------
 
             // Save Sub Pages list
@@ -471,17 +326,10 @@ public class PageAdminList extends AbstractPageObject {
             file.flush();
             file.close();
 
-            FileWriter fileModule = new FileWriter(sPathToFile + sDataFileModulesJson);
-            fileModule.write(jsonObjectModule.toJSONString().replace("\\", ""));
-            fileModule.flush();
-            fileModule.close();
-
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
-            pageNames = false;
-        } catch (IOException e2) {
-            e2.printStackTrace();
-            pageNames = false;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return jsonObject;
