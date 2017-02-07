628654cbd98821fc699a8665dfd7305477ed8220
philip_sushkov <philips@q4websystems.com>
2017-02-07 11:57:18 -0500

    Alert Filter Add added - 1

---
+++ src/test/java/pageobjects/SystemAdmin/AlertFilterList/AlertFilterAdd.java
@@ -0,0 +1,15 @@
+package pageobjects.SystemAdmin.AlertFilterList;
+
+import org.openqa.selenium.WebDriver;
+import pageobjects.AbstractPageObject;
+
+/**
+ * Created by philipsushkov on 2017-02-07.
+ */
+public class AlertFilterAdd extends AbstractPageObject {
+
+    public AlertFilterAdd(WebDriver driver) {
+        super(driver);
+    }
+
+}
