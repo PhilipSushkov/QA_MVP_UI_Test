185afd2630761041ef2fa31fede3062d1bc89951
philip_sushkov <pylypsushkov@gmail.com>
2017-04-10 15:37:08 -0400

    UserGroupAdd has been started

---
+++ src/test/java/pageobjects/SystemAdmin/UserGroupList/UserGroupAdd.java
@@ -0,0 +1,7 @@
+package pageobjects.SystemAdmin.UserGroupList;
+
+/**
+ * Created by philipsushkov on 2017-04-10.
+ */
+public class UserGroupAdd {
+}
---
+++ src/test/java/specs/SystemAdmin/UserGroupList/CheckUserGroupAdd.java
@@ -0,0 +1,25 @@
+package specs.SystemAdmin.UserGroupList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pageobjects.Dashboard.Dashboard;
+import pageobjects.LoginPage.LoginPage;
+import specs.AbstractSpec;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by philipsushkov on 2017-04-10.
+ */
+
+public class CheckUserGroupAdd {
+
+}
