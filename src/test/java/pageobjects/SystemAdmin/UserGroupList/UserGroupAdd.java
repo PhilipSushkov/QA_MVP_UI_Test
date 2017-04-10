a8f25987a59c5160f812c37883311fbf1caa4b4c
philip_sushkov <pylypsushkov@gmail.com>
2017-04-10 16:22:05 -0400

    UserGroupAdd has been started

--- src/test/java/pageobjects/SystemAdmin/UserGroupList/UserGroupAdd.java
+++ src/test/java/pageobjects/SystemAdmin/UserGroupList/UserGroupAdd.java
@@ -1,7 +1,75 @@
 package pageobjects.SystemAdmin.UserGroupList;
 
+import com.jayway.jsonpath.JsonPath;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import pageobjects.AbstractPageObject;
+import util.Functions;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
+import static specs.AbstractSpec.desktopUrl;
+import static specs.AbstractSpec.propUISystemAdmin;
+
 /**
  * Created by philipsushkov on 2017-04-10.
  */
-public class UserGroupAdd {
+
+public class UserGroupAdd extends AbstractPageObject {
+    private static By moduleTitle;
+    private static By addNewLink;
+    private static By activeChk, saveBtn, cancelBtn, deleteBtn;
+    private static String sPathToFile, sFileJson;
+    private static JSONParser parser;
+    private static final long DEFAULT_PAUSE = 2500;
+    private final String PAGE_NAME="User Group";
+
+    public UserGroupAdd(WebDriver driver) {
+        super(driver);
+        moduleTitle = By.xpath(propUISystemAdmin.getProperty("spanModule_Title"));
+        addNewLink = By.xpath(propUISystemAdmin.getProperty("input_AddNew"));
+        activeChk = By.xpath(propUISystemAdmin.getProperty("chk_Active"));
+        saveBtn = By.xpath(propUISystemAdmin.getProperty("btn_Save"));
+        cancelBtn = By.xpath(propUISystemAdmin.getProperty("btn_Cancel"));
+        deleteBtn = By.xpath(propUISystemAdmin.getProperty("btn_Delete"));
+        //successMsg = By.xpath(propUISystemAdmin.getProperty("msg_Success"));
+
+        parser = new JSONParser();
+
+        sPathToFile = System.getProperty("user.dir") + propUISystemAdmin.getProperty("dataPath_UserGroupList");
+        sFileJson = propUISystemAdmin.getProperty("json_UserGroups");
+    }
+
+    public String getTitle() {
+        findElement(addNewLink).click();
+        waitForElement(moduleTitle);
+        String sTitle = getText(moduleTitle);
+        findElement(cancelBtn).click();
+        return sTitle;
+    }
+
+    public String saveUserGroup(JSONObject data, String name) {
+
+        return "User Group List";
+    }
+
+    public String getPageUrl (JSONObject obj, String name) {
+        String  sFilterId = JsonPath.read(obj, "$.['"+name+"'].url_query.UserID");
+        String  sLanguageId = JsonPath.read(obj, "$.['"+name+"'].url_query.LanguageId");
+        String  sSectionId = JsonPath.read(obj, "$.['"+name+"'].url_query.SectionId");
+        return desktopUrl.toString()+"default.aspx?UserID="+sFilterId+"&LanguageId="+sLanguageId+"&SectionId="+sSectionId;
+    }
 }
--- src/test/java/specs/SystemAdmin/UserGroupList/CheckUserGroupAdd.java
+++ src/test/java/specs/SystemAdmin/UserGroupList/CheckUserGroupAdd.java
@@ -9,6 +9,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import pageobjects.Dashboard.Dashboard;
 import pageobjects.LoginPage.LoginPage;
+import pageobjects.SystemAdmin.UserGroupList.UserGroupAdd;
 import specs.AbstractSpec;
 
 import java.io.FileNotFoundException;
@@ -20,6 +21,86 @@ import java.util.ArrayList;
  * Created by philipsushkov on 2017-04-10.
  */
 
-public class CheckUserGroupAdd {
+public class CheckUserGroupAdd extends AbstractSpec {
+    private static By systemAdminMenuButton, userGroupListMenuItem;
+    private static LoginPage loginPage;
+    private static Dashboard dashboard;
+    private static UserGroupAdd userGroupAdd;
+
+    private static String sPathToFile, sDataFileJson;
+    private static JSONParser parser;
+
+    private final String DATA="getData", USER_GROUP_NAME="user_group_name", PAGE_NAME="User Group";
+
+    @BeforeTest
+    public void setUp() throws Exception {
+        systemAdminMenuButton = By.xpath(propUISystemAdmin.getProperty("btnMenu_SystemAdmin"));
+        userGroupListMenuItem = By.xpath(propUISystemAdmin.getProperty("itemMenu_UserGroupList"));
+
+        loginPage = new LoginPage(driver);
+        dashboard = new Dashboard(driver);
+        userGroupAdd = new UserGroupAdd(driver);
+
+        sPathToFile = System.getProperty("user.dir") + propUISystemAdmin.getProperty("dataPath_UserGroupList");
+        sDataFileJson = propUISystemAdmin.getProperty("json_UserGroupData");
+
+        parser = new JSONParser();
+
+        loginPage.loginUser();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() throws Exception {
+        dashboard.openPageFromMenu(systemAdminMenuButton, userGroupListMenuItem);
+    }
+
+    @Test(dataProvider=DATA, priority=1)
+    public void saveUser(JSONObject data) {
+        String sUserGroupName = data.get(USER_GROUP_NAME).toString();
+        String expectedTitleList = "User Group List";
+        String expectedTitleEdit = "User Group Edit";
+
+        Assert.assertEquals(userGroupAdd.getTitle(), expectedTitleEdit, "Actual "+PAGE_NAME+" Edit page Title doesn't match to expected");
+        Assert.assertEquals(userGroupAdd.saveUserGroup(data, sUserGroupName), expectedTitleList, "New "+PAGE_NAME+" didn't save properly");
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+
+        try {
+            JSONObject jsonObject = (JSONObject) parser.parse(new FileReader(sPathToFile + sDataFileJson));
+            JSONArray jsonArray = (JSONArray) jsonObject.get("user_group");
+            ArrayList<Object> zoom = new ArrayList();
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JSONObject pageObj = (JSONObject) jsonArray.get(i);
+                if (Boolean.parseBoolean(pageObj.get("do_assertions").toString())) {
+                    zoom.add(jsonArray.get(i));
+                }
+            }
+
+            Object[][] data = new Object[zoom.size()][1];
+            for (int i = 0; i < zoom.size(); i++) {
+                data[i][0] = zoom.get(i);
+            }
+
+            return data;
+
+        }  catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @AfterTest
+    public void tearDown() {
+        dashboard.logoutFromAdmin();
+        //driver.quit();
+    }
 
 }
