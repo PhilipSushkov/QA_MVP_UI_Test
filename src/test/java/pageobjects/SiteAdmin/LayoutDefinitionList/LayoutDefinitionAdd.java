015b88049158041ec14ceedf093af97c14224e03
philip_sushkov <pylypsushkov@gmail.com>
2017-04-25 14:23:16 -0400

    CheckLayoutDefinitionAdd have been finished

--- src/test/java/pageobjects/SiteAdmin/LayoutDefinitionList/LayoutDefinitionAdd.java
+++ src/test/java/pageobjects/SiteAdmin/LayoutDefinitionList/LayoutDefinitionAdd.java
@@ -60,8 +60,8 @@ public class LayoutDefinitionAdd extends AbstractPageObject {
 
         parser = new JSONParser();
 
-        sPathToFile = System.getProperty("user.dir") + propUISiteAdmin.getProperty("dataPath_GlobalModuleList");
-        sFileJson = propUISiteAdmin.getProperty("json_GlobalModules");
+        sPathToFile = System.getProperty("user.dir") + propUISiteAdmin.getProperty("dataPath_LayoutDefinitionList");
+        sFileJson = propUISiteAdmin.getProperty("json_LayoutDefinitions");
     }
 
     public String getTitle() {
@@ -101,8 +101,7 @@ public class LayoutDefinitionAdd extends AbstractPageObject {
             jsonObj.put("layout_type", layout_type);
             if (layout_type.equals("Default Layout")) {
                 findElement(layoutPathRb).click();
-            }
-            if (layout_type.equals("Custom Layout")) {
+            } else if (layout_type.equals("Custom Layout")) {
                 findElement(userControlContentRb).click();
             }
 
@@ -240,7 +239,422 @@ public class LayoutDefinitionAdd extends AbstractPageObject {
         return null;
     }
 
-    public String getPageUrl (JSONObject obj, String name) {
+    public Boolean checkLayoutDefinition(JSONObject data, String name) throws InterruptedException {
+        JSONObject jsonMain = new JSONObject();
+
+        try {
+            try {
+                FileReader readFile = new FileReader(sPathToFile + sFileJson);
+                jsonMain = (JSONObject) parser.parse(readFile);
+            } catch (ParseException e) {
+            }
+
+            String pageUrl = getPageUrl(jsonMain, name);
+            driver.get(pageUrl);
+            Thread.sleep(DEFAULT_PAUSE);
+            waitForElement(commentsTxt);
+
+            // Compare field values with entry data
+            try {
+                if (findElement(friendlyNameField).toString().equals(data.get("friendly_name").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                if (findElement(descriptionField).toString().equals(data.get("description").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                if (!new Select(findElement(defaultLayoutSelect)).getFirstSelectedOption().getText().equals(data.get("default_layout").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                if (Boolean.parseBoolean(findElement(layoutPathRb).getAttribute("checked"))) {
+                    if (!data.get("layout_type").toString().equals("Default Layout")) {
+                        return false;
+                    }
+                } else if (Boolean.parseBoolean(findElement(userControlContentRb).getAttribute("checked"))) {
+                    if (!data.get("layout_type").toString().equals("Custom Layout")) {
+                        return false;
+                    }
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                if (!findElement(isAdminChk).getAttribute("checked").equals(data.get("is_admin").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                if (!findElement(activeChk).getAttribute("checked").equals(data.get("active").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                if (!findElement(viewStateChk).getAttribute("checked").equals(data.get("enable_viewState").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            }
+
+
+            System.out.println(name+ ": New "+PAGE_NAME+" has been checked");
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String publishLayoutDefinition(JSONObject data, String name) throws InterruptedException {
+        JSONObject jsonMain = new JSONObject();
+        JSONObject jsonObj = new JSONObject();
+
+        try {
+            try {
+                FileReader readFile = new FileReader(sPathToFile + sFileJson);
+                jsonMain = (JSONObject) parser.parse(readFile);
+                jsonObj = (JSONObject) jsonMain.get(name);
+            } catch (ParseException e) {
+            }
+
+            String pageUrl = getPageUrl(jsonMain, name);
+            driver.get(pageUrl);
+            Thread.sleep(DEFAULT_PAUSE);
+
+            waitForElement(publishBtn);
+            findElement(publishBtn).click();
+            Thread.sleep(DEFAULT_PAUSE*2);
+
+            driver.get(pageUrl);
+            Thread.sleep(DEFAULT_PAUSE*2);
+            driver.navigate().refresh();
+            Thread.sleep(DEFAULT_PAUSE);
+
+            jsonObj.put("workflow_state", WorkflowState.LIVE.state());
+            jsonObj.put("deleted", "false");
+
+            jsonMain.put(name, jsonObj);
+
+            FileWriter file = new FileWriter(sPathToFile + sFileJson);
+            file.write(jsonMain.toJSONString().replace("\\", ""));
+            file.flush();
+
+            Thread.sleep(DEFAULT_PAUSE*2);
+            driver.navigate().refresh();
+
+            System.out.println(name+ ": New "+PAGE_NAME+" has been published");
+            return findElement(workflowStateSpan).getText();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String changeAndSubmitLayoutDefinition(JSONObject data, String name) throws InterruptedException {
+        JSONObject jsonMain = new JSONObject();
+        JSONObject jsonObj = new JSONObject();
+
+        try {
+            try {
+                FileReader readFile = new FileReader(sPathToFile + sFileJson);
+                jsonMain = (JSONObject) parser.parse(readFile);
+                jsonObj = (JSONObject) jsonMain.get(name);
+            } catch (ParseException e) {
+            }
+
+            String pageUrl = getPageUrl(jsonMain, name);
+            driver.get(pageUrl);
+            Thread.sleep(DEFAULT_PAUSE);
+            waitForElement(saveAndSubmitBtn);
+
+            try {
+                if (!data.get("default_layout_ch").toString().isEmpty()) {
+                    findElement(defaultLayoutSelect).sendKeys(data.get("default_layout_ch").toString());
+                    jsonObj.put("default_layout", data.get("default_layout_ch").toString());
+                }
+            } catch (NullPointerException e) {
+            }
+
+            jsonObj.put("active", Boolean.parseBoolean(data.get("active").toString()));
+            try {
+                // Save Active checkbox
+                if (Boolean.parseBoolean(data.get("active_ch").toString())) {
+                    if (!Boolean.parseBoolean(findElement(activeChk).getAttribute("checked"))) {
+                        findElement(activeChk).click();
+                        jsonObj.put("active", true);
+                    } else {
+                    }
+                } else {
+                    if (!Boolean.parseBoolean(findElement(activeChk).getAttribute("checked"))) {
+                    } else {
+                        findElement(activeChk).click();
+                        jsonObj.put("active", false);
+                    }
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                findElement(commentsTxt).clear();
+                findElement(commentsTxt).sendKeys(data.get("comment_ch").toString());
+            } catch (NullPointerException e) {
+            }
+
+            findElement(saveAndSubmitBtn).click();
+            Thread.sleep(DEFAULT_PAUSE);
+
+            jsonObj.put("workflow_state", WorkflowState.FOR_APPROVAL.state());
+            jsonObj.put("deleted", "false");
+
+            jsonMain.put(name, jsonObj);
+
+            FileWriter file = new FileWriter(sPathToFile + sFileJson);
+            file.write(jsonMain.toJSONString().replace("\\", ""));
+            file.flush();
+
+            driver.get(pageUrl);
+            Thread.sleep(DEFAULT_PAUSE);
+            waitForElement(workflowStateSpan);
+
+            System.out.println(name+ ": New "+PAGE_NAME+" changes have been submitted");
+            return findElement(workflowStateSpan).getText();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String revertToLiveLayoutDefinition(String name) throws InterruptedException {
+        JSONObject jsonMain = new JSONObject();
+        JSONObject jsonObj = new JSONObject();
+
+        try {
+            try {
+                FileReader readFile = new FileReader(sPathToFile + sFileJson);
+                jsonMain = (JSONObject) parser.parse(readFile);
+                jsonObj = (JSONObject) jsonMain.get(name);
+            } catch (ParseException e) {
+            }
+
+            String pageUrl = getPageUrl(jsonMain, name);
+            driver.get(pageUrl);
+            Thread.sleep(DEFAULT_PAUSE);
+            waitForElement(revertBtn);
+
+            if (jsonObj.get("workflow_state").toString().equals(WorkflowState.FOR_APPROVAL.state())) {
+                findElement(revertBtn).click();
+                Thread.sleep(DEFAULT_PAUSE);
+
+                jsonObj.put("workflow_state", WorkflowState.LIVE.state());
+                jsonObj.put("deleted", "false");
+
+                jsonMain.put(name, jsonObj);
+
+                FileWriter file = new FileWriter(sPathToFile + sFileJson);
+                file.write(jsonMain.toJSONString().replace("\\", ""));
+                file.flush();
+
+                driver.get(pageUrl);
+                Thread.sleep(DEFAULT_PAUSE);
+                waitForElement(workflowStateSpan);
+
+                System.out.println(name+ ": "+PAGE_NAME+" has been reverted to Live");
+                return findElement(workflowStateSpan).getText();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public Boolean checkLayoutDefinitionCh(JSONObject data, String name) throws InterruptedException {
+        JSONObject jsonMain = new JSONObject();
+
+        try {
+            try {
+                FileReader readFile = new FileReader(sPathToFile + sFileJson);
+                jsonMain = (JSONObject) parser.parse(readFile);
+            } catch (ParseException e) {
+            }
+
+            String pageUrl = getPageUrl(jsonMain, name);
+            driver.get(pageUrl);
+            Thread.sleep(DEFAULT_PAUSE);
+            waitForElement(commentsTxt);
+
+            // Compare field values with entry data
+            try {
+                if (findElement(friendlyNameField).toString().equals(data.get("friendly_name").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                if (findElement(descriptionField).toString().equals(data.get("description").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                if (!new Select(findElement(defaultLayoutSelect)).getFirstSelectedOption().getText().equals(data.get("default_layout_ch").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                if (Boolean.parseBoolean(findElement(layoutPathRb).getAttribute("checked"))) {
+                    if (!data.get("layout_type").toString().equals("Default Layout")) {
+                        return false;
+                    }
+                } else if (Boolean.parseBoolean(findElement(userControlContentRb).getAttribute("checked"))) {
+                    if (!data.get("layout_type").toString().equals("Custom Layout")) {
+                        return false;
+                    }
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                if (!findElement(isAdminChk).getAttribute("checked").equals(data.get("is_admin").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                if (!findElement(activeChk).getAttribute("checked").equals(data.get("active_ch").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            }
+
+            try {
+                if (!findElement(viewStateChk).getAttribute("checked").equals(data.get("enable_viewState").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            }
+
+
+            System.out.println(name+ ": New "+PAGE_NAME+" has been checked");
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String setupAsDeletedLayoutDefinition(String name) throws InterruptedException {
+        JSONObject jsonMain = new JSONObject();
+        JSONObject jsonObj = new JSONObject();
+
+        try {
+            try {
+                FileReader readFile = new FileReader(sPathToFile + sFileJson);
+                jsonMain = (JSONObject) parser.parse(readFile);
+                jsonObj = (JSONObject) jsonMain.get(name);
+            } catch (ParseException e) {
+            }
+
+            String pageUrl = getPageUrl(jsonMain, name);
+            driver.get(pageUrl);
+            Thread.sleep(DEFAULT_PAUSE);
+
+            waitForElement(commentsTxt);
+            findElement(commentsTxt).sendKeys("Removing "+PAGE_NAME);
+            findElement(deleteBtn).click();
+            Thread.sleep(DEFAULT_PAUSE);
+
+            driver.get(pageUrl);
+            Thread.sleep(DEFAULT_PAUSE);
+            waitForElement(currentContentSpan);
+
+            jsonObj.put("workflow_state", WorkflowState.FOR_APPROVAL.state());
+            jsonObj.put("deleted", "true");
+
+            jsonMain.put(name, jsonObj);
+
+            FileWriter file = new FileWriter(sPathToFile + sFileJson);
+            file.write(jsonMain.toJSONString().replace("\\", ""));
+            file.flush();
+
+            System.out.println(name+ ": "+PAGE_NAME+" set up as deleted");
+            return findElement(currentContentSpan).getText();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String removeLayoutDefinition(JSONObject data, String name) throws InterruptedException {
+        JSONObject jsonMain = new JSONObject();
+
+        try {
+            try {
+                FileReader readFile = new FileReader(sPathToFile + sFileJson);
+                jsonMain = (JSONObject) parser.parse(readFile);
+            } catch (ParseException e) {
+            }
+
+            String pageUrl = getPageUrl(jsonMain, name);
+            driver.get(pageUrl);
+            Thread.sleep(DEFAULT_PAUSE);
+
+            if (findElement(currentContentSpan).getText().equals(WorkflowState.DELETE_PENDING.state())) {
+
+                waitForElement(commentsTxt);
+                findElement(commentsTxt).sendKeys("Approving "+PAGE_NAME+" removal");
+                findElement(publishBtn).click();
+
+                Thread.sleep(DEFAULT_PAUSE*2);
+
+                driver.get(pageUrl);
+                Thread.sleep(DEFAULT_PAUSE);
+
+                jsonMain.remove(name);
+
+                FileWriter file = new FileWriter(sPathToFile + sFileJson);
+                file.write(jsonMain.toJSONString().replace("\\", ""));
+                file.flush();
+
+                Thread.sleep(DEFAULT_PAUSE*2);
+                driver.navigate().refresh();
+
+                System.out.println(name+ ": New "+PAGE_NAME+" has been removed");
+                return findElement(workflowStateSpan).getText();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String getPageUrl(JSONObject obj, String name) {
         String  sItemID = JsonPath.read(obj, "$.['"+name+"'].url_query.ItemID");
         String  sLanguageId = JsonPath.read(obj, "$.['"+name+"'].url_query.LanguageId");
         String  sSectionId = JsonPath.read(obj, "$.['"+name+"'].url_query.SectionId");
