a2de133042a5fd37d0870d0732fd18ed3e586ff6
philip_sushkov <pylypsushkov@gmail.com>
2017-05-03 14:11:00 -0400

    CheckLinkToPageAdd has been started

---
+++ src/test/java/pageobjects/SiteAdmin/LinkToPageList/LinkToPageAdd.java
@@ -0,0 +1,9 @@
+package pageobjects.SiteAdmin.LinkToPageList;
+
+/**
+ * Created by philipsushkov on 2017-05-03.
+ */
+
+public class LinkToPageAdd {
+
+}
---
+++ src/test/java/specs/SiteAdmin/LinkToPageList/CheckLinkToPageAdd.java
@@ -0,0 +1,9 @@
+package specs.SiteAdmin.LinkToPageList;
+
+/**
+ * Created by philipsushkov on 2017-05-03.
+ */
+
+public class CheckLinkToPageAdd {
+
+}
