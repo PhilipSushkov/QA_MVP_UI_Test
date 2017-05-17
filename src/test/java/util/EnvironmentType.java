da1b3cdfd4e1426752998ed0c6c008b94db8e8f3
philip_sushkov <pylypsushkov@gmail.com>
2017-05-17 16:32:19 -0400

    few small changes

--- src/test/java/util/EnvironmentType.java
+++ src/test/java/util/EnvironmentType.java
@@ -3,11 +3,11 @@ package util;
 public enum EnvironmentType {
 
   DEVELOP("aestest.s1.q4web.newtest/admin/"),
-  BETA("chicagotest.s1.q4web.release/admin/"), //chicagotest url
+  //BETA("chicagotest.s1.q4web.release/admin/"), //chicagotest url
   //BETA("test1.s1.q4preview.com/admin/"),
   //BETA("maxwell2016ir.s1.q4web.release/"),
   //BETA("facebook.s1.q4web.newtest/admin/"),
-  //BETA("chicagotest.s3.q4web.com/admin/"),
+  BETA("chicagotest.s1.q4web.newtest"),
   //BETA("cornerstone.s1.q4web.newtest/admin/"),
 
   //BETA("goldcorptest.s1.q4web.newtest/admin/"), //goldcorp url
