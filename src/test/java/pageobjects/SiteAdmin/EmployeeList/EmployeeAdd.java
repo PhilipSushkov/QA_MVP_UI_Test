595460f328dc35d6ae334ce18d62fa9f8ce5d718
Andy Park <hg4park@uwaterloo.ca>
2017-06-05 15:15:58 -0400

    Added Employee Edit

--- src/test/java/pageobjects/SiteAdmin/EmployeeList/EmployeeAdd.java
+++ src/test/java/pageobjects/SiteAdmin/EmployeeList/EmployeeAdd.java
@@ -6,12 +6,14 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 import pageobjects.AbstractPageObject;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 
 import static specs.AbstractSpec.propUISiteAdmin;
 
@@ -27,7 +29,7 @@ public class EmployeeAdd extends AbstractPageObject {
     private static String sPathToFile, sFileJson;
     private static JSONParser parser;
     private static final long DEFAULT_PAUSE = 2500;
-    private final String PAGE_NAME="External Feed";
+    private final String PAGE_NAME="Employee List", EMPLOYEE_NAME = "Bob";
 
     public EmployeeAdd(WebDriver driver) {
         super(driver);
@@ -163,6 +165,228 @@ public class EmployeeAdd extends AbstractPageObject {
 
         return null;
     }
+    public Boolean checkEmployeeList(JSONObject data, String name) {
+        JSONObject jsonMain = new JSONObject();
+        JSONObject jsonObj = new JSONObject();
+        String email;
 
+        try {
+            FileReader readFile = new FileReader(sPathToFile + sFileJson);
+            jsonMain = (JSONObject) parser.parse(readFile);
+            jsonObj = (JSONObject) jsonMain.get(name);
+        } catch (ParseException e) {
+        } catch (IOException e) {
+        }
 
+        email = data.get("email").toString();
+
+        By editBtn = By.xpath("//td[(text()='" + email + "')]/parent::tr/td/input[contains(@id, 'btnEdit')]");
+
+        try {
+            waitForElement(moduleTitle);
+            findElement(editBtn).click();
+            waitForElement(saveBtn);
+
+            try{
+                if(findElement(emailInput).toString().equals(data.get("email").toString())){
+                    return false;
+                }
+            } catch (NullPointerException e) {
+            return false;
+            }
+
+            try{
+                if(findElement(passwordInput).toString().equals(data.get("password").toString())){
+                    return false;
+                }
+            } catch (NullPointerException e) {
+                return false;
+            }
+
+            try{
+                if(findElement(firstNameInput).toString().equals(data.get("first_name").toString())){
+                    return false;
+                }
+            } catch (NullPointerException e) {
+                return false;
+            }
+
+            try{
+                if(findElement(lastNameInput).toString().equals(data.get("last_name").toString())){
+                    return false;
+                }
+            } catch (NullPointerException e) {
+                return false;
+            }
+
+            try{
+                if(findElement(jobTitleInput).toString().equals(data.get("job_title").toString())){
+                    return false;
+                }
+            } catch (NullPointerException e) {
+                return false;
+            }
+
+            try{
+                if(findElement(phoneInput).toString().equals(data.get("phone").toString())){
+                    return false;
+                }
+            } catch (NullPointerException e) {
+                return false;
+            }
+
+            try{
+                if(findElement(extensionInput).toString().equals(data.get("extension").toString())){
+                    return false;
+                }
+            } catch (NullPointerException e) {
+                return false;
+            }
+
+            try{
+                if(findElement(cellPhoneInput).toString().equals(data.get("cell_phone").toString())){
+                    return false;
+                }
+            } catch (NullPointerException e) {
+                return false;
+            }
+
+            try{
+                if(findElement(locationInput).toString().equals(data.get("location").toString())){
+                    return false;
+                }
+            } catch (NullPointerException e) {
+                return false;
+            }
+
+            try {
+                if (!findElement(activeChk).getAttribute("checked").equals(data.get("active").toString())) {
+                    return false;
+                }
+            } catch (NullPointerException e) {
+                return false;
+            }
+
+            // Save Url
+            URL url = new URL(getUrl());
+            String[] params = url.getQuery().split("&");
+            JSONObject jsonURLQuery = new JSONObject();
+            for (String param:params) {
+                jsonURLQuery.put(param.split("=")[0], param.split("=")[1]);
+            }
+            jsonObj.put("url_query", jsonURLQuery);
+
+            int ExternalFeedID = Integer.parseInt(jsonURLQuery.get("UserID").toString());
+
+            try {
+                FileWriter writeFile = new FileWriter(sPathToFile + sFileJson);
+                writeFile.write(jsonMain.toJSONString().replace("\\", ""));
+                writeFile.flush();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(name + ": "+PAGE_NAME+" has been checked");
+            return ExternalFeedID > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public Boolean editExternalFeed(JSONObject data, String name) throws InterruptedException {
+        JSONObject jsonMain = new JSONObject();
+        JSONObject jsonObj = new JSONObject();
+        String feed, details_name = null;
+
+        try {
+            FileReader readFile = new FileReader(sPathToFile + sFileJson);
+            jsonMain = (JSONObject) parser.parse(readFile);
+            jsonObj = (JSONObject) jsonMain.get(name);
+        } catch (ParseException e) {
+        } catch (IOException e) {
+        }
+
+        feed = data.get("feed").toString();
+
+        switch (feed) {
+            case PRESS_RELEASE:
+                details_name = "ClientId: " + jsonObj.get("comp_id").toString();
+                break;
+
+            case EOD_MAIL:
+                details_name = jsonObj.get("stock_exchange").toString() + " : " + jsonObj.get("stock_symbol").toString();
+                break;
+
+            default:
+                break;
+        }
+
+        By editBtn = By.xpath("//td[(text()='" + details_name + "')]/parent::tr/td/input[contains(@id, 'btnEdit')]");
+
+        try {
+
+            waitForElement(moduleTitle);
+            String pageUrl = getPageUrl(jsonMain, name);
+            //driver.get(pageUrl);
+            findElement(editBtn).click();
+            waitForElement(deleteBtn);
+
+            try {
+                if (!data.get("tag_list_ch").toString().isEmpty()) {
+                    String tag_list_ch = data.get("tag_list_ch").toString();
+                    findElement(tagListInput).clear();
+                    findElement(tagListInput).sendKeys(tag_list_ch);
+                    jsonObj.put("tag_list", tag_list_ch);
+                }
+            } catch (NullPointerException e) {}
+
+            try {
+                if (!data.get("active_ch").toString().isEmpty()) {
+                    if (Boolean.parseBoolean(data.get("active_ch").toString())) {
+                        if (!Boolean.parseBoolean(findElement(activeChk).getAttribute("checked"))) {
+                            findElement(activeChk).click();
+                            jsonObj.put("active", true);
+                        } else {
+                        }
+                    } else {
+                        if (!Boolean.parseBoolean(findElement(activeChk).getAttribute("checked"))) {
+                        } else {
+                            findElement(activeChk).click();
+                            jsonObj.put("active", false);
+                        }
+                    }
+                }
+            } catch (NullPointerException e) {
+            }
+
+            Thread.sleep(DEFAULT_PAUSE);
+            findElement(saveBtn).click();
+
+            try {
+                FileWriter writeFile = new FileWriter(sPathToFile + sFileJson);
+                writeFile.write(jsonMain.toJSONString().replace("\\", ""));
+                writeFile.flush();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Thread.sleep(DEFAULT_PAUSE);
+            waitForElement(editBtn);
+
+            System.out.println(name + ": " + PAGE_NAME + " has been changed");
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 }
--- src/test/java/specs/SiteAdmin/EmployeeList/CheckEmployeeAdd.java
+++ src/test/java/specs/SiteAdmin/EmployeeList/CheckEmployeeAdd.java
@@ -1,11 +1,14 @@
 package specs.SiteAdmin.EmployeeList;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pageobjects.Dashboard.Dashboard;
 import pageobjects.LoginPage.LoginPage;
@@ -13,6 +16,11 @@ import pageobjects.SiteAdmin.EmployeeList.EmployeeAdd;
 import pageobjects.SiteAdmin.EmployeeList.EmployeeList;
 import pageobjects.SiteAdmin.ExternalFeedList.ExternalFeedAdd;
 import specs.AbstractSpec;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by andyp on 2017-05-29.
@@ -27,7 +35,7 @@ public class CheckEmployeeAdd extends AbstractSpec{
     private static String sPathToFile, sDataFileJson;
     private static JSONParser parser;
 
-    private final String DATA="getData", EMPLOYEE_NAME = "Bob", PAGE_NAME="Employee List";
+    private final String DATA="getData", EMPLOYEE_EMAIL = "email", PAGE_NAME="Employee List";
 
     @BeforeTest
     public void setUp() throws Exception {
@@ -53,12 +61,58 @@ public class CheckEmployeeAdd extends AbstractSpec{
     @Test(dataProvider=DATA, priority=1)
     public void saveEmployeeList(JSONObject data) {
         //Checking Employee List vs. Employee Edit
-        String sEmployeeName = data.get(EMPLOYEE_NAME).toString();
+        String sEmployeeEmail = data.get(EMPLOYEE_EMAIL).toString();
         String expectedTitleList = "Employee List";
         String expectedTitleEdit = "Employee Edit";
 
         Assert.assertEquals(employeeAdd.getTitle(), expectedTitleEdit, "Actual "+PAGE_NAME+" Edit page Title doesn't match to expected");
-        Assert.assertEquals(employeeAdd.saveEmployeeList(data, sEmployeeName), expectedTitleList, "New "+PAGE_NAME+" didn't save properly");
+        Assert.assertEquals(employeeAdd.saveEmployeeList(data, sEmployeeEmail), expectedTitleList, "New "+PAGE_NAME+" didn't save properly");
+    }
+
+    @Test(dataProvider=DATA, priority=2)
+    public void checkEmployeeList(JSONObject data) {
+        String sEmployeeEmail = data.get(EMPLOYEE_EMAIL).toString();
+        Assert.assertTrue(employeeAdd.checkEmployeeList(data, sEmployeeEmail), "New "+PAGE_NAME+" doesn't fit to entry data (after Save)");
+    }
+
+    @Test(dataProvider=DATA, priority=3)
+    public void editExternalFeed(JSONObject data) throws Exception {
+        String sEmployeeEmail = data.get(EMPLOYEE_EMAIL).toString();
+
+        Assert.assertTrue(employeeAdd.editExternalFeed(data, sEmployeeEmail), PAGE_NAME+" didn't change properly (after Save)");
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+
+        try {
+            JSONObject jsonObject = (JSONObject) parser.parse(new FileReader(sPathToFile + sDataFileJson));
+            JSONArray jsonArray = (JSONArray) jsonObject.get("employee_list");
+            ArrayList<Object> zoom = new ArrayList();
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JSONObject pageObj = (JSONObject) jsonArray.get(i);
+                if (Boolean.parseBoolean(pageObj.get("do_assertions").toString())) {
+                    zoom.add(jsonArray.get(i));
+                }
+            }
+
+            Object[][] data = new Object[zoom.size()][1];
+            for (int i = 0; i < zoom.size(); i++) {
+                data[i][0] = zoom.get(i);
+            }
+
+            return data;
+
+        }  catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     //Save Employee
