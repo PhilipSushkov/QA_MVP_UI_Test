5c7b4e3168cf488f5ce0634434e7dae844999930
philip_sushkov <pylypsushkov@gmail.com>
2017-06-07 16:22:53 -0400

    CheckFinancialReportAdd has been started

---
+++ src/test/java/pageobjects/ContentAdmin/FinancialReports/FinancialReportAdd.java
@@ -0,0 +1,9 @@
+package pageobjects.ContentAdmin.FinancialReports;
+
+/**
+ * Created by philipsushkov on 2017-06-07.
+ */
+
+public class FinancialReportAdd {
+
+}
---
+++ src/test/java/specs/ContentAdmin/FinancialReports/CheckFinancialReportAdd.java
@@ -0,0 +1,9 @@
+package specs.ContentAdmin.FinancialReports;
+
+/**
+ * Created by philipsushkov on 2017-06-07.
+ */
+
+public class CheckFinancialReportAdd {
+
+}
