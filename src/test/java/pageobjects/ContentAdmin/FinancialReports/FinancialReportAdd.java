f07f26a75d1cddf7ef7032b5c4d1017c401b3807
philip_sushkov <pylypsushkov@gmail.com>
2017-06-07 16:55:22 -0400

    CheckFinancialReportAdd has been started - 2

--- src/test/java/pageobjects/ContentAdmin/FinancialReports/FinancialReportAdd.java
+++ src/test/java/pageobjects/ContentAdmin/FinancialReports/FinancialReportAdd.java
@@ -1,9 +1,32 @@
 package pageobjects.ContentAdmin.FinancialReports;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import pageobjects.AbstractPageObject;
+
+import com.jayway.jsonpath.JsonPath;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import pageobjects.PageAdmin.WorkflowState;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+
+import static specs.AbstractSpec.desktopUrl;
+import static specs.AbstractSpec.propUIContentAdmin;
+
 /**
  * Created by philipsushkov on 2017-06-07.
  */
 
-public class FinancialReportAdd {
+public class FinancialReportAdd extends AbstractPageObject {
+
+    public FinancialReportAdd(WebDriver driver) {
+        super(driver);
+    }
 
 }
--- src/test/java/specs/ContentAdmin/FinancialReports/CheckFinancialReportAdd.java
+++ src/test/java/specs/ContentAdmin/FinancialReports/CheckFinancialReportAdd.java
@@ -1,9 +1,54 @@
 package specs.ContentAdmin.FinancialReports;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pageobjects.ContentAdmin.FinancialReports.FinancialReportAdd;
+import pageobjects.Dashboard.Dashboard;
+import pageobjects.LoginPage.LoginPage;
+import pageobjects.PageAdmin.WorkflowState;
+import specs.AbstractSpec;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Created by philipsushkov on 2017-06-07.
  */
 
-public class CheckFinancialReportAdd {
+public class CheckFinancialReportAdd extends AbstractSpec {
+    private static By contentAdminMenuButton, financialReportsMenuItem;
+    private static LoginPage loginPage;
+    private static Dashboard dashboard;
+    private static FinancialReportAdd financialReportAdd;
+
+    private static String sPathToFile, sDataFileJson;
+    private static JSONParser parser;
+
+    private final String DATA="getData", PAGE_NAME="Financial Report";
+    private final String FINANCIAL_REPORT_YEAR="report_year", FINANCIAL_REPORT_TYPE="report_type";
+
+    @BeforeTest
+    public void setUp() throws Exception {
+        contentAdminMenuButton = By.xpath(propUIContentAdmin.getProperty("btnMenu_ContentAdmin"));
+        financialReportsMenuItem = By.xpath(propUIContentAdmin.getProperty("btnMenu_FinancialReports"));
+
+        loginPage = new LoginPage(driver);
+        dashboard = new Dashboard(driver);
+        financialReportAdd = new FinancialReportAdd(driver);
+
+        sPathToFile = System.getProperty("user.dir") + propUISiteAdmin.getProperty("dataPath_FinancialReportList");
+        sDataFileJson = propUISiteAdmin.getProperty("json_FinancialReportData");
+
+        parser = new JSONParser();
+
+        loginPage.loginUser();
+    }
 
 }
