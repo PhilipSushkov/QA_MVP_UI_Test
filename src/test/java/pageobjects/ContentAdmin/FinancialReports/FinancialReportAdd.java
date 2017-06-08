85d1ac4004fcb30792ea550e6b18f4a351ecbd1f
philip_sushkov <pylypsushkov@gmail.com>
2017-06-08 10:35:57 -0400

    SaveFinancialReport has been started - 1

--- src/test/java/pageobjects/ContentAdmin/FinancialReports/FinancialReportAdd.java
+++ src/test/java/pageobjects/ContentAdmin/FinancialReports/FinancialReportAdd.java
@@ -24,9 +24,122 @@ import static specs.AbstractSpec.propUIContentAdmin;
  */
 
 public class FinancialReportAdd extends AbstractPageObject {
+    private static By moduleTitle;
+    private static By activeChk, saveBtn, revertBtn, cancelBtn, deleteBtn, addNewLink, publishBtn;
+    private static By workflowStateSpan, commentsTxt, successMsg, saveAndSubmitBtn, currentContentSpan;
+    private static String sPathToFile, sFileJson;
+    private static JSONParser parser;
+    private static final long DEFAULT_PAUSE = 2500;
+    private final String PAGE_NAME="Financial Report";
 
     public FinancialReportAdd(WebDriver driver) {
         super(driver);
+        moduleTitle = By.xpath(propUIContentAdmin.getProperty("spanModule_Title"));
+        activeChk = By.xpath(propUIContentAdmin.getProperty("chk_Active"));
+        saveBtn = By.xpath(propUIContentAdmin.getProperty("btn_Save"));
+        cancelBtn = By.xpath(propUIContentAdmin.getProperty("btn_Cancel"));
+        deleteBtn = By.xpath(propUIContentAdmin.getProperty("btn_Delete"));
+        publishBtn = By.xpath(propUIContentAdmin.getProperty("btn_Publish"));
+        addNewLink = By.xpath(propUIContentAdmin.getProperty("input_AddNew"));
+        revertBtn = By.xpath(propUIContentAdmin.getProperty("btn_Revert"));
+        workflowStateSpan = By.xpath(propUIContentAdmin.getProperty("select_WorkflowState"));
+        commentsTxt = By.xpath(propUIContentAdmin.getProperty("txtarea_Comments"));
+        successMsg = By.xpath(propUIContentAdmin.getProperty("msg_Success"));
+        saveAndSubmitBtn = By.xpath(propUIContentAdmin.getProperty("btn_SaveAndSubmit"));
+        currentContentSpan = By.xpath(propUIContentAdmin.getProperty("span_CurrentContent"));
+
+        parser = new JSONParser();
+
+        sPathToFile = System.getProperty("user.dir") + propUIContentAdmin.getProperty("dataPath_LookupList");
+        sFileJson = propUIContentAdmin.getProperty("json_Lookup");
+    }
+
+    public String getTitle() {
+        findElement(addNewLink).click();
+        waitForElement(moduleTitle);
+        String sTitle = getText(moduleTitle);
+        findElement(cancelBtn).click();
+        return sTitle;
+    }
+
+    public String saveFinancialReport(JSONObject data, String name) {
+        String lookup_type, lookup_text, lookup_value, additional_info;
+        Boolean active;
+        JSONObject jsonObj = new JSONObject();
+        JSONObject jsonMain = new JSONObject();
+
+        waitForElement(addNewLink);
+        findElement(addNewLink).click();
+        waitForElement(saveBtn);
+
+        try {
+            try {
+                FileReader readFile = new FileReader(sPathToFile + sFileJson);
+                jsonMain = (JSONObject) parser.parse(readFile);
+            } catch (ParseException e) {
+            }
+
+            lookup_type = data.get("lookup_type").toString();
+            findElement(lookupTypeField).clear();
+            findElement(lookupTypeField).sendKeys(lookup_type);
+            jsonObj.put("lookup_type", lookup_type);
+
+            lookup_text = data.get("lookup_text").toString();
+            findElement(lookupTextField).clear();
+            findElement(lookupTextField).sendKeys(lookup_text);
+            jsonObj.put("lookup_text", lookup_text);
+
+            lookup_value = data.get("lookup_value").toString();
+            findElement(lookupValueField).clear();
+            findElement(lookupValueField).sendKeys(lookup_value);
+            jsonObj.put("lookup_value", lookup_value);
+
+            additional_info = data.get("additional_info").toString();
+            findElement(additionalInfoField).clear();
+            findElement(additionalInfoField).sendKeys(additional_info);
+            jsonObj.put("additional_info", additional_info);
+
+            // Save Active checkbox
+            active = Boolean.parseBoolean(data.get("active").toString());
+            jsonObj.put("active", Boolean.parseBoolean(data.get("active").toString()));
+            if (active) {
+                if (!Boolean.parseBoolean(findElement(activeChk).getAttribute("checked"))) {
+                    findElement(activeChk).click();
+                } else {
+                }
+            } else {
+                if (!Boolean.parseBoolean(findElement(activeChk).getAttribute("checked"))) {
+                } else {
+                    findElement(activeChk).click();
+                }
+            }
+
+            findElement(saveBtn).click();
+            Thread.sleep(DEFAULT_PAUSE);
+            waitForElement(successMsg);
+
+            jsonObj.put("workflow_state", WorkflowState.IN_PROGRESS.state());
+
+            jsonMain.put(name, jsonObj);
+
+            try {
+                FileWriter writeFile = new FileWriter(sPathToFile + sFileJson);
+                writeFile.write(jsonMain.toJSONString().replace("\\", ""));
+                writeFile.flush();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(name + ": "+PAGE_NAME+" has been created");
+            return findElement(workflowStateSpan).getText();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
 }
--- src/test/java/specs/ContentAdmin/FinancialReports/CheckFinancialReportAdd.java
+++ src/test/java/specs/ContentAdmin/FinancialReports/CheckFinancialReportAdd.java
@@ -31,7 +31,7 @@ public class CheckFinancialReportAdd extends AbstractSpec {
     private static String sPathToFile, sDataFileJson;
     private static JSONParser parser;
 
-    private final String DATA="getData", PAGE_NAME="Financial Report";
+    private final String DATA="getData", PAGE_NAME="Financial Report", ANNUAL_REPORT="Annual Report";
     private final String FINANCIAL_REPORT_YEAR="report_year", FINANCIAL_REPORT_TYPE="report_type";
 
     @BeforeTest
@@ -49,6 +49,67 @@ public class CheckFinancialReportAdd extends AbstractSpec {
         parser = new JSONParser();
 
         loginPage.loginUser();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() throws Exception {
+        dashboard.openPageFromMenu(contentAdminMenuButton, financialReportsMenuItem);
+    }
+
+    @Test(dataProvider=DATA, priority=1)
+    public void saveLookup(JSONObject data) {
+        String sFinancialReportTitle;
+        String sFinancialReportYear = data.get(FINANCIAL_REPORT_YEAR).toString();
+        String sFinancialReportType = data.get(FINANCIAL_REPORT_TYPE).toString();
+
+        if (sFinancialReportType.equals(ANNUAL_REPORT)) {
+            sFinancialReportTitle = sFinancialReportYear + " " + sFinancialReportType;
+        } else {
+            sFinancialReportTitle = sFinancialReportType + " " + sFinancialReportYear;
+        }
+
+        String expectedTitleEdit = "Financial Report Edit";
+
+        Assert.assertEquals(financialReportAdd.getTitle(), expectedTitleEdit, "Actual "+PAGE_NAME+" Edit page Title doesn't match to expected");
+        Assert.assertEquals(financialReportAdd.saveFinancialReport(data, sFinancialReportTitle), WorkflowState.IN_PROGRESS.state(), "New "+PAGE_NAME+" didn't save properly");
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+
+        try {
+            JSONObject jsonObject = (JSONObject) parser.parse(new FileReader(sPathToFile + sDataFileJson));
+            JSONArray jsonArray = (JSONArray) jsonObject.get("financial_report");
+            ArrayList<Object> zoom = new ArrayList();
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JSONObject pageObj = (JSONObject) jsonArray.get(i);
+                if (Boolean.parseBoolean(pageObj.get("do_assertions").toString())) {
+                    zoom.add(jsonArray.get(i));
+                }
+            }
+
+            Object[][] data = new Object[zoom.size()][1];
+            for (int i = 0; i < zoom.size(); i++) {
+                data[i][0] = zoom.get(i);
+            }
+
+            return data;
+
+        }  catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @AfterTest
+    public void tearDown() {
+        dashboard.logoutFromAdmin();
     }
 
 }
