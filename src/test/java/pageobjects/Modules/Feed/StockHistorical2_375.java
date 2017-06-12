88b4d5044b2c8512db80be113f01adbfe86fd692
philip_sushkov <pylypsushkov@gmail.com>
2017-06-12 10:56:10 -0400

    Modules have been started

---
+++ src/test/java/pageobjects/Modules/Feed/StockHistorical2_375.java
@@ -0,0 +1,7 @@
+package pageobjects.Modules.Feed.json;
+
+/**
+ * Created by philipsushkov on 2017-06-12.
+ */
+public class StockHistorical2_375 {
+}
---
+++ src/test/java/pageobjects/Modules/PressRelease/PressRelease.java
@@ -0,0 +1,7 @@
+package pageobjects.Modules.PressRelease.json;
+
+/**
+ * Created by philipsushkov on 2017-06-12.
+ */
+public class PressRelease {
+}
