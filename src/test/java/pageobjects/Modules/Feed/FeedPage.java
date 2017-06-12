810f443e3cc50466b5e5cc2871cc0ef7026e6ee6
philip_sushkov <pylypsushkov@gmail.com>
2017-06-12 11:37:21 -0400

    CreateFeedPage have been started

--- src/test/java/pageobjects/Modules/Feed/FeedPage.java
+++ src/test/java/pageobjects/Modules/Feed/FeedPage.java
@@ -1,7 +1,75 @@
 package pageobjects.Modules.Feed;
 
+import com.jayway.jsonpath.JsonPath;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.openqa.selenium.*;
+import pageobjects.AbstractPageObject;
+import pageobjects.PageAdmin.PageAdminList;
+import util.Functions;
+
+import static specs.AbstractSpec.desktopUrl;
+import static specs.AbstractSpec.propUIPageAdmin;
+
 /**
  * Created by philipsushkov on 2017-06-12.
  */
-public class FeedPage {
+
+public class FeedPage extends AbstractPageObject {
+    private static By addNewBtn, backBtn, sectionTitleInput, pageTypeInternalRd, pageTypeExternalRd, externalURLInput, publishBtn;
+    private static By pageTemplateSelect, parentPageSelect, showNavChk, openNewWindChk, saveBtn, workflowStateSpan, currentContentSpan;
+    private static By revertBtn, parentUrlSpan, seoNameInput, previewLnk, breadcrumbDiv, commentsTxt, deleteBtn, addNewInput;
+    private static By saveAndSubmitBtn, rejectBtn;
+    private static String sPathToFile, sFilePagesJson, sFileModulesJson;
+    private static JSONParser parser;
+    private static final long DEFAULT_PAUSE = 2500;
+    private static PageAdminList pageAdminList;
+
+    public FeedPage(WebDriver driver) {
+        super(driver);
+
+        addNewBtn = By.xpath(propUIPageAdmin.getProperty("btn_AddNew"));
+        sectionTitleInput = By.xpath(propUIPageAdmin.getProperty("input_SectionTitle"));
+        pageTypeInternalRd = By.xpath(propUIPageAdmin.getProperty("rd_PageTypeInt"));
+        pageTypeExternalRd = By.xpath(propUIPageAdmin.getProperty("rd_PageTypeExt"));
+        externalURLInput = By.xpath(propUIPageAdmin.getProperty("input_ExternalURL"));
+        pageTemplateSelect = By.xpath(propUIPageAdmin.getProperty("select_PageTemplate"));
+        parentPageSelect = By.xpath(propUIPageAdmin.getProperty("select_ParentPage"));
+        showNavChk = By.xpath(propUIPageAdmin.getProperty("chk_ShowInNav"));
+        openNewWindChk = By.xpath(propUIPageAdmin.getProperty("chk_OpenInNewWindow"));
+        workflowStateSpan = By.xpath(propUIPageAdmin.getProperty("select_WorkflowState"));
+        parentUrlSpan = By.xpath(propUIPageAdmin.getProperty("span_YourPageUrl"));
+        seoNameInput = By.xpath(propUIPageAdmin.getProperty("input_SeoName"));
+        previewLnk = By.xpath(propUIPageAdmin.getProperty("lnk_Preview"));
+        breadcrumbDiv = By.xpath(propUIPageAdmin.getProperty("div_Breadcrumb"));
+        commentsTxt = By.xpath(propUIPageAdmin.getProperty("txtarea_Comments"));
+        addNewInput = By.xpath(propUIPageAdmin.getProperty("input_AddNew"));
+        currentContentSpan = By.xpath(propUIPageAdmin.getProperty("span_CurrentContent"));
+
+        saveBtn = By.xpath(propUIPageAdmin.getProperty("btn_Save"));
+        revertBtn = By.xpath(propUIPageAdmin.getProperty("btn_Revert"));
+        deleteBtn = By.xpath(propUIPageAdmin.getProperty("btn_Delete"));
+        publishBtn = By.xpath(propUIPageAdmin.getProperty("btn_Publish"));
+        backBtn = By.xpath(propUIPageAdmin.getProperty("btn_Back"));
+        saveAndSubmitBtn = By.xpath(propUIPageAdmin.getProperty("btn_SaveAndSubmit"));
+        rejectBtn = By.xpath(propUIPageAdmin.getProperty("btn_Reject"));
+
+        sPathToFile = System.getProperty("user.dir") + propUIPageAdmin.getProperty("dataPath_PageAdmin");
+        sFilePagesJson = propUIPageAdmin.getProperty("json_PagesProp");
+        sFileModulesJson = propUIPageAdmin.getProperty("json_ModulesProp");
+
+        parser = new JSONParser();
+        pageAdminList = new PageAdminList(driver);;
+    }
+
 }
--- src/test/java/specs/Modules/Feed/CreateFeedPage.java
+++ src/test/java/specs/Modules/Feed/CreateFeedPage.java
@@ -1,9 +1,98 @@
 package specs.Modules.Feed;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+import org.openqa.selenium.By;
+import pageobjects.Dashboard.Dashboard;
+import pageobjects.LoginPage.LoginPage;
+import pageobjects.Modules.Feed.FeedPage;
+import pageobjects.PageAdmin.PageAdd;
+import pageobjects.PageAdmin.WorkflowState;
+import specs.AbstractSpec;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Created by philipsushkov on 2017-06-12.
  */
 
-public class CreateFeedPage {
+public class CreateFeedPage extends AbstractSpec {
+    private static By pageAdminMenuButton;
+    private static LoginPage loginPage;
+    private static Dashboard dashboard;
+    private static FeedPage feedPage;
+
+    private static String sPathToFile, sDataFileJson;
+    private static JSONParser parser;
+
+    private final String PAGE_DATA="pageData", SECTION_TITLE="section_title";
+
+    @BeforeTest
+    public void setUp() throws Exception {
+        pageAdminMenuButton = By.xpath(propUIPageAdmin.getProperty("btnMenu_PageAdmin"));
+
+        loginPage = new LoginPage(driver);
+        dashboard = new Dashboard(driver);
+        feedPage = new FeedPage(driver);
+
+        sPathToFile = System.getProperty("user.dir") + propUIPageAdmin.getProperty("dataPath_ModulesFeed");
+        sDataFileJson = propUIPageAdmin.getProperty("json_CreatePageData");
+
+        parser = new JSONParser();
+
+        loginPage.loginUser();
+    }
+
+
+    @DataProvider
+    public Object[][] pageData() {
+
+        try {
+            JSONObject jsonObject = (JSONObject) parser.parse(new FileReader(sPathToFile + sDataFileJson));
+            JSONArray pageData = (JSONArray) jsonObject.get("pages");
+            ArrayList<Object> zoom = new ArrayList();
+
+            for (int i = 0; i < pageData.size(); i++) {
+                JSONObject pageObj = (JSONObject) pageData.get(i);
+                if (Boolean.parseBoolean(pageObj.get("do_assertions").toString())) {
+                    zoom.add(pageData.get(i));
+                }
+            }
+
+            Object[][] newPages = new Object[zoom.size()][1];
+            for (int i = 0; i < zoom.size(); i++) {
+                newPages[i][0] = zoom.get(i);
+            }
+
+            return newPages;
+
+        }  catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    @AfterTest
+    public void tearDown() {
+        dashboard.logoutFromAdmin();
+        //driver.quit();
+    }
 
 }
