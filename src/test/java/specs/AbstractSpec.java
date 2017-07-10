bf2eb4c79e3eebe985c03f8707f8f0844bcc2912
Danny Liu <danny98liu@gmail.com>
2017-07-10 10:58:28 -0400

    Faq Test

--- src/test/java/specs/AbstractSpec.java
+++ src/test/java/specs/AbstractSpec.java
@@ -79,6 +79,8 @@ public abstract class AbstractSpec extends util.Functions {
     public static Properties propUIModulesPresentation;
     private static final String PATHTO_MODULESEVENT_PROP = "Modules/Event/Event.properties";
     public static Properties propUIModulesEvent;
+    private static final String PATHTO_MODULESFAQ_PROP = "Modules/Faq/Faq.properties";
+    public static Properties propUIModulesFaq;
     private static final String PATHTO_API_PROP = "api/ApiMap.properties";
     public static Properties propAPI;
 
@@ -266,6 +268,7 @@ public abstract class AbstractSpec extends util.Functions {
         propUIModulesPressRelease = ConnectToPropUI(PATHTO_MODULESPRESSRELEASE_PROP);
         propUIModulesPresentation = ConnectToPropUI(PATHTO_MODULESPRESENTATION_PROP);
         propUIModulesEvent = ConnectToPropUI(PATHTO_MODULESEVENT_PROP);
+        propUIModulesFaq = ConnectToPropUI(PATHTO_MODULESFAQ_PROP);
         propAPI = ConnectToPropUI(PATHTO_API_PROP);
     }
 
