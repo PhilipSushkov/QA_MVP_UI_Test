c6485a8b39364e7f97582f318dcbb0a654abcc2b
philip_sushkov <pylypsushkov@gmail.com>
2017-07-18 11:27:13 -0400

    CheckDdosAttack.class has been finished

--- src/test/java/pageobjects/api/DdosAttack.java
+++ src/test/java/pageobjects/api/DdosAttack.java
@@ -11,9 +11,15 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import util.Functions;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.URI;
 import java.net.URLEncoder;
+import java.util.Date;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,32 +30,77 @@ import java.util.logging.Logger;
 
 public class DdosAttack {
     private HttpClient client;
-    //private String URL = "facebookrelease.q4web.release";
-    private String URL = "chicagotest.q4web.com";
+    private String URL = "facebookrelease.q4web.release";
+    //private String URL = "chicagotest.q4web.com";
     private String PROTOCOL = "http://";
+    private int REF_NUM = 5;
 
-    public void DdosAttackRequest(String sUrl) {
+    public void DdosAttackRequest(String sUrl, JSONObject data, FileWriter writer) {
 
         //To hide warnings logs from execution console.
         Logger logger = Logger.getLogger("");
         logger.setLevel(Level.OFF);
 
-        try {
-            //client = HttpClientBuilder.create().build();
+        JSONArray referrals = (JSONArray) data.get("referrals");
 
-            int randNum = Functions.randInt(5, 50);
-            String query = "+{[=-&*#~";
-            final String alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-            final int N = alphabet.length();
+        try {
+            int statusCode = 0;
+
+            int randNum = Functions.randInt(10, 50);
+            Random r = new Random();
+            String queryQ4 = "";
+            String queryR = "";
+            //final String alphabetQ4 = "                                                       0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+{_[=-&*#~+:;`]|}!@^";
+            final String alphabetR = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+{_[=-&*#~+:;`]|}!@^";
+            final int Nq4 = alphabetR.length();
+            final int Nr = alphabetR.length();
 
             for (int i=0; i<randNum; i++) {
-                Random r = new Random();
-                query = query + alphabet.charAt(r.nextInt(N));
+                queryQ4 = queryQ4 + alphabetR.charAt(r.nextInt(Nq4));
+                queryR = queryR + alphabetR.charAt(r.nextInt(Nr));
             }
 
-            System.out.println(URLEncoder.encode(query, "UTF-8"));
-            String fullURL = PROTOCOL+URL+sUrl+query;
-            HttpGet request = new HttpGet(fullURL+"+");
+            String sReferralSite = referrals.get(r.nextInt(REF_NUM)).toString();
+            String sReferralUrl = sReferralSite + sUrl + queryR;
+            System.out.println(sReferralUrl);
+
+            //String queryEncoded = URLEncoder.encode(queryQ4, "UTF-8");
+            //System.out.println(URLEncoder.encode(queryQ4, "UTF-8"));
+            String fullURL = PROTOCOL+URL+"/"+queryQ4;
+            System.out.println(fullURL);
+
+            Socket socket = new Socket(URL, 80);
+
+            PrintWriter request = new PrintWriter(socket.getOutputStream());
+            request.print(  "GET " + "/" + queryQ4 + " HTTP/1.1\r\n" +
+                    "Host: " + URL + "\r\n" +
+                    "Accept: text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01\r\n" +
+                    "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36\r\n" +
+                    "Accept-Language: en-US,en;q=0.8,ru;q=0.6\r\n" +
+                    "Cookie: __unam=8c5bdf9-1580728caca-14a17300-70; ASP.NET_SessionId=0puw3swnsinu3jojxdo55b25; facebookrelease.q4web.release_Q4_ASPX_PUBLIC_LanguageId=1; _gat=1; _ga=GA1.2.1148342226.1477588633; _gid=GA1.2.453586935.1500321226\r\n" +
+                    "Referer: " + sReferralUrl + "\r\n" +
+                    "Connection: keep-alive\r\n\r\n");
+
+            long start = System.currentTimeMillis();
+            request.flush();
+            InputStream inStream = socket.getInputStream();
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(inStream));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                //System.out.println(line);
+                if (line.contains("HTTP/1.1 404 Not Found")) {
+                    statusCode = 404;
+                } else if (line.contains("HTTP/1.1 400 Bad Request")) {
+                    statusCode = 400;
+                }
+            }
+            rd.close();
+            long end = System.currentTimeMillis();
+            long result = end-start;
+
+            /*
+            HttpGet request = new HttpGet(fullURL);
 
             request.setHeader(HttpHeaders.HOST, URL);
             request.setHeader(HttpHeaders.CONTENT_TYPE, "text/html; charset=utf-8");
@@ -73,19 +124,38 @@ public class DdosAttack {
             long start = System.currentTimeMillis();
             HttpResponse response = client.execute(request, localContext);
             long end = System.currentTimeMillis();
-            System.out.println("Round trip response time = " + (end-start) + " millis");
+            long result = end-start;
+            System.out.println("Round trip response time = " + result + " millis");
 
             int statusCode = response.getStatusLine().getStatusCode();
 
             System.out.println(fullURL+": "+statusCode);
+            */
 
+            /*
             Header[] headers = response.getAllHeaders();
             for (Header header : headers) {
                 System.out.println("Key : " + header.getName()
                         + " ,Value : " + header.getValue());
             }
             System.out.println("\n Done");
+            */
 
+            try
+            {
+                writer.append(fullURL);
+                writer.append(',');
+                writer.append(Long.toString(result));
+                writer.append(',');
+                writer.append(sReferralUrl);
+                writer.append(',');
+                writer.append(Long.toString(statusCode));
+                writer.append('\n');
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/test/java/specs/api/CheckDdosAttack.java
+++ src/test/java/specs/api/CheckDdosAttack.java
@@ -4,6 +4,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -12,9 +13,11 @@ import specs.ApiAbstractSpec;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by philipsushkov on 2017-07-17.
@@ -25,11 +28,22 @@ public class CheckDdosAttack extends ApiAbstractSpec {
     private String sPathToFileDdos, sDataFileDdosJson;
     private JSONParser parser;
     private final int threadPoolSize = 1, iterations = 1;
+    private FileWriter writer;
 
     @BeforeTest
     public void setUp() throws IOException {
         sPathToFileDdos = System.getProperty("user.dir") + propAPI.getProperty("dataPath_Ddos");
         sDataFileDdosJson = propAPI.getProperty("jsonData_Ddos");
+        writer = new FileWriter("ddos_test_"+new Date()+".csv",false);
+
+        writer.append("Request");
+        writer.append(',');
+        writer.append("Time");
+        writer.append(',');
+        writer.append("Referral");
+        writer.append(',');
+        writer.append("Response code");
+        writer.append('\n');
     }
 
     @DataProvider(parallel = true)
@@ -68,9 +82,17 @@ public class CheckDdosAttack extends ApiAbstractSpec {
     @Test(dataProvider = "DdosAtackData", threadPoolSize = threadPoolSize)
     public void CheckDdosAttack(JSONObject data) throws IOException {
         String sUrl = data.get("url").toString();
+
         for (int i=0; i<iterations; i++) {
-            new DdosAttack().DdosAttackRequest(sUrl);
+            new DdosAttack().DdosAttackRequest(sUrl, data, writer);
         }
+
+    }
+
+    @AfterTest
+    public void tearDown() throws IOException {
+        writer.flush();
+        writer.close();
     }
 
 }
