72335d3b2f76ef0c71d5d2f1aa0134ff578cc0e5
philip_sushkov <pylypsushkov@gmail.com>
2017-07-19 13:48:14 -0400

    CheckFinancialReportAdd has been finished - 4

--- src/test/java/pageobjects/Modules/Feed/StockHistorical.java
+++ src/test/java/pageobjects/Modules/Feed/StockHistorical.java
@@ -12,13 +12,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.openqa.selenium.*;
 import pageobjects.AbstractPageObject;
-import pageobjects.PageAdmin.PageAdminList;
 import pageobjects.PageAdmin.WorkflowState;
-import util.Functions;
 
 import static specs.AbstractSpec.*;
 
@@ -28,11 +25,17 @@ import static specs.AbstractSpec.*;
 
 public class StockHistorical extends AbstractPageObject {
     private static By addNewModuleBtn, backBtn, moduleTitleInput, moduleDefinitionSelect, includeLagacyModulesChk;
-    private static By publishBtn, saveBtn, workflowStateSpan, currentContentSpan, propertiesHref;
+    private static By publishBtn, saveBtn, workflowStateSpan, currentContentSpan, propertiesHref, previewLnk;
     private static By commentsTxt, deleteBtn, saveAndSubmitBtn, regionNameSelect;
     private static String sPathToPageFile, sFilePageJson, sPathToModuleFile, sFileModuleJson;
     private static JSONParser parser;
+
     private static final long DEFAULT_PAUSE = 2500;
+
+    // This test checks specific historical data on the following day:
+    private static final String lookupMonth = "Jul";
+    private static final String lookupDay = "1";
+    private static final String lookupYear = "2015";
 
     public StockHistorical(WebDriver driver) {
         super(driver);
@@ -46,6 +49,7 @@ public class StockHistorical extends AbstractPageObject {
         commentsTxt = By.xpath(propUIPageAdmin.getProperty("txtarea_Comments"));
         currentContentSpan = By.xpath(propUIPageAdmin.getProperty("span_CurrentContent"));
         propertiesHref = By.xpath(propUIModules.getProperty("href_Properties"));
+        previewLnk = By.xpath(propUIModules.getProperty("lnk_Preview"));
 
         saveBtn = By.xpath(propUIPageAdmin.getProperty("btn_Save"));
         deleteBtn = By.xpath(propUIPageAdmin.getProperty("btn_Delete"));
@@ -56,93 +60,9 @@ public class StockHistorical extends AbstractPageObject {
         sPathToPageFile = System.getProperty("user.dir") + propUIModules.getProperty("dataPath_Modules");
         sFilePageJson = propUIModules.getProperty("json_PagesProp");
         sPathToModuleFile = System.getProperty("user.dir") + propUIModulesFeed.getProperty("dataPath_Feed");
-        sFileModuleJson = propUIModulesFeed.getProperty("json_DtockHistorical2_375Prop");
+        sFileModuleJson = propUIModulesFeed.getProperty("json_StockHistoricalProp");
 
         parser = new JSONParser();
-    }
-
-    public String saveModule(JSONObject modulesDataObj, String moduleName) throws InterruptedException {
-        String module_title, module_definition, region_name;
-
-        try {
-
-            JSONObject jsonObj = (JSONObject) parser.parse(new FileReader(sPathToPageFile + sFilePageJson));
-
-            String pageUrl = getPageUrl(jsonObj, moduleName);
-            driver.get(pageUrl);
-            Thread.sleep(DEFAULT_PAUSE);
-
-            waitForElement(commentsTxt);
-
-            findElement(addNewModuleBtn).click();
-            Thread.sleep(DEFAULT_PAUSE);
-            waitForElement(includeLagacyModulesChk);
-
-            findElement(includeLagacyModulesChk).click();
-            Thread.sleep(DEFAULT_PAUSE);
-            waitForElement(moduleTitleInput);
-
-            JSONObject module = new JSONObject();
-
-            module_title = modulesDataObj.get("module_title").toString();
-            findElement(moduleTitleInput).sendKeys(module_title);
-            module.put("module_title", module_title);
-
-            module_definition = modulesDataObj.get("module_definition").toString();
-            findElement(moduleDefinitionSelect).sendKeys(module_definition);
-            module.put("module_definition", module_definition);
-
-            region_name = modulesDataObj.get("region_name").toString();
-            findElement(regionNameSelect).sendKeys(region_name);
-            module.put("region_name", region_name);
-
-            Thread.sleep(DEFAULT_PAUSE);
-
-            findElement(saveBtn).click();
-            Thread.sleep(DEFAULT_PAUSE);
-            waitForElement(deleteBtn);
-
-
-            module.put("workflow_state", WorkflowState.IN_PROGRESS.state());
-            module.put("active", "true");
-
-            URL pageURL = new URL(getUrl());
-            String[] params = pageURL.getQuery().split("&");
-            JSONObject jsonURLQuery = new JSONObject();
-            for (String param:params) {
-                jsonURLQuery.put(param.split("=")[0], param.split("=")[1]);
-            }
-            module.put("url_query", jsonURLQuery);
-
-            JSONObject jsonObjSave;
-            try {
-                jsonObjSave = (JSONObject) parser.parse(new FileReader(sPathToModuleFile + sFileModuleJson));
-            } catch (ParseException e) {
-                jsonObjSave = (JSONObject) parser.parse("{}");
-            }
-
-            jsonObjSave.put(module_title, module);
-
-            try {
-                FileWriter file = new FileWriter(sPathToModuleFile + sFileModuleJson);
-                file.write(jsonObjSave.toJSONString().replace("\\", ""));
-                file.flush();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            System.out.println("New "+moduleName+" has been created");
-            return findElement(workflowStateSpan).getText();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 
     public String saveAndSubmitModule(JSONObject modulesDataObj, String moduleName) throws InterruptedException {
@@ -163,15 +83,12 @@ public class StockHistorical extends AbstractPageObject {
             Thread.sleep(DEFAULT_PAUSE);
 
             for (int i=0; i<jsonArrProp.size(); i++) {
-                //System.out.println(jsonArrProp.get(i).toString());
-                //String prop[] = jsonArrProp.get(i).toString().split(";");
-                //System.out.println(prop[0]);
                 By propertyValue = By.xpath("//td[contains(@class, 'DataGridItemBorderLeft')][(text()='"+jsonArrProp.get(i).toString().split(";")[0]+"')]/parent::tr/td/div/input[contains(@id, 'txtStatic')]");
                 findElement(propertyValue).clear();
                 findElement(propertyValue).sendKeys(jsonArrProp.get(i).toString().split(";")[1]);
             }
 
-            findElement(commentsTxt).sendKeys(modulesDataObj.get("comment_module").toString());
+            findElement(commentsTxt).sendKeys(modulesDataObj.get("comment").toString());
             findElement(saveAndSubmitBtn).click();
             Thread.sleep(DEFAULT_PAUSE);
 
@@ -195,131 +112,19 @@ public class StockHistorical extends AbstractPageObject {
             e.printStackTrace();
         }
 
-
-        return "For Approval";
-    }
-
-    public String publishModule(String moduleName) throws InterruptedException {
-        try {
-            JSONObject jsonObj = (JSONObject) parser.parse(new FileReader(sPathToModuleFile + sFileModuleJson));
-
-            String moduleUrl = getModuleUrl(jsonObj, moduleName);
-            driver.get(moduleUrl);
-            Thread.sleep(DEFAULT_PAUSE);
-
-            waitForElement(publishBtn);
-            findElement(publishBtn).click();
-            Thread.sleep(DEFAULT_PAUSE*2);
-
-            driver.get(moduleUrl);
-            Thread.sleep(DEFAULT_PAUSE);
-
-            JSONObject moduleObj = (JSONObject) jsonObj.get(moduleName);
-
-            moduleObj.put("workflow_state", WorkflowState.LIVE.state());
-
-            jsonObj.put(moduleName, moduleObj);
-
-            FileWriter file = new FileWriter(sPathToModuleFile + sFileModuleJson);
-            file.write(jsonObj.toJSONString().replace("\\", ""));
-            file.flush();
-
-
-            Thread.sleep(DEFAULT_PAUSE*2);
-            driver.navigate().refresh();
-
-            System.out.println("New "+moduleName+" has been published");
-            return findElement(workflowStateSpan).getText();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
         return null;
     }
 
-    public String setupAsDeletedModule(String moduleName) throws InterruptedException {
-        try {
-            JSONObject jsonObj = (JSONObject) parser.parse(new FileReader(sPathToModuleFile + sFileModuleJson));
+    public void lookupHistoricalValue(JSONObject module) {
+        By lookupMonthSelect = By.xpath(module.get("module_path").toString() + propUIModulesFeed.getProperty("select_LookupMonth"));
+        By lookupDaySelect = By.xpath(module.get("module_path").toString() + propUIModulesFeed.getProperty("select_LookupDay"));
+        By lookupYearSelect = By.xpath(module.get("module_path").toString() + propUIModulesFeed.getProperty("select_LookupYear"));
+        By lookupBtn = By.xpath(module.get("module_path").toString() + propUIModulesFeed.getProperty("btn_Lookup"));
 
-            String moduleUrl = getModuleUrl(jsonObj, moduleName);
-            driver.get(moduleUrl);
-            Thread.sleep(DEFAULT_PAUSE);
-
-            waitForElement(commentsTxt);
-            findElement(commentsTxt).sendKeys("Removing the module");
-            findElement(deleteBtn).click();
-
-            Thread.sleep(DEFAULT_PAUSE);
-
-            driver.get(moduleUrl);
-            waitForElement(currentContentSpan);
-
-            JSONObject moduleObj = (JSONObject) jsonObj.get(moduleName);
-
-            moduleObj.put("workflow_state", WorkflowState.FOR_APPROVAL.state());
-
-            jsonObj.put(moduleName, moduleObj);
-
-            FileWriter file = new FileWriter(sPathToModuleFile + sFileModuleJson);
-            file.write(jsonObj.toJSONString().replace("\\", ""));
-            file.flush();
-
-            System.out.println(moduleName+ ": Module set up as deleted");
-            return findElement(currentContentSpan).getText();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    public String removeModule(JSONObject modulesDataObj, String moduleName) throws InterruptedException {
-        try {
-            JSONObject jsonObj = (JSONObject) parser.parse(new FileReader(sPathToModuleFile + sFileModuleJson));
-
-            String moduleUrl = getModuleUrl(jsonObj, moduleName);
-            driver.get(moduleUrl);
-            Thread.sleep(DEFAULT_PAUSE);
-
-            if (findElement(currentContentSpan).getText().equals(WorkflowState.DELETE_PENDING.state())) {
-
-                waitForElement(commentsTxt);
-                findElement(commentsTxt).sendKeys("Approving the module removal");
-                findElement(publishBtn).click();
-
-                Thread.sleep(DEFAULT_PAUSE*2);
-
-                driver.get(moduleUrl);
-                Thread.sleep(DEFAULT_PAUSE);
-
-                jsonObj.remove(moduleName);
-
-                FileWriter file = new FileWriter(sPathToModuleFile + sFileModuleJson);
-                file.write(jsonObj.toJSONString().replace("\\", ""));
-                file.flush();
-
-                Thread.sleep(DEFAULT_PAUSE*2);
-                driver.navigate().refresh();
-
-                System.out.println(moduleName+ ": Module has been removed");
-                return findElement(workflowStateSpan).getText();
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return null;
+        findElement(lookupMonthSelect).sendKeys(lookupMonth);
+        findElement(lookupDaySelect).sendKeys(lookupDay);
+        findElement(lookupYearSelect).sendKeys(lookupYear);
+        findElement(lookupBtn).click();
     }
 
     private String getPageUrl(JSONObject obj, String moduleName) {
