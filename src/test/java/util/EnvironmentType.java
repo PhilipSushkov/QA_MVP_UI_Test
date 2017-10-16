f6dae1b3ccb06ff6c100969f2beac37bf7109f7d
philip_sushkov <pylypsushkov@gmail.com>
2017-10-16 09:27:29 -0400

    few changes - 101617-1

--- src/test/java/util/EnvironmentType.java
+++ src/test/java/util/EnvironmentType.java
@@ -2,18 +2,18 @@ package util;
 
 public enum EnvironmentType {
 
-  //BETA("facebook.s1.q4web.newtest/"), //chicagotest url
-  //BETA("qachicagotest.s1.q4web.newtest/admin/"), //facebookrelease url
-  //DEVELOP("fbreverseproxy.s1.q4web.dev/admin/"),
-//  DEVELOP("ensco.s1.q4web.newtest/admin/"),
+  // BETA("facebook.s1.q4web.newtest/"), //chicagotest url
+  // BETA("qachicagotest.s1.q4web.newtest/admin/"), //facebookrelease url
+  // DEVELOP("fbreverseproxy.s1.q4web.dev/admin/"),
+  // DEVELOP("ensco.s1.q4web.newtest/admin/"),
   DEVELOP("qaensco.s1.q4web.newtest/admin/"),
-  //DEVELOP("facebook.q4web.newtest/login.aspx"),
-  //DEVELOP("chicagotest.s3.q4web.newtest/admin/"),
-  //DEVELOP("enscoreverseproxy.s1.q4web.dev/admin/"),
-  //BETA("chicagotest.s1.q4web.newtest/admin/"), //chicagotest url
+  // DEVELOP("facebook.q4web.newtest/login.aspx"),
+  // DEVELOP("chicagotest.s3.q4web.newtest/admin/"),
+  // DEVELOP("enscoreverseproxy.s1.q4web.dev/admin/"),
+  // BETA("chicagotest.s1.q4web.newtest/admin/"), //chicagotest url
   BETA("ensco.s1.q4web.newtest/admin"), //facebookrelease url
   PRODUCTION("chicagotest.s3.q4preview.com/admin/");
-  //DEVELOP("admin-dev.q4inc.com/");
+  // DEVELOP("admin-dev.q4inc.com/");
 
   private final String host;
   private final String protocol = "https://";
