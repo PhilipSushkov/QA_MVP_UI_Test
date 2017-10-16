27a7aef1f24eafb0c40a777ffd41a548f0ad94e9
philip_sushkov <pylypsushkov@gmail.com>
2017-10-16 12:11:04 -0400

    few changes - 101617-1

--- src/test/java/specs/api/CheckCacheStatus.java
+++ src/test/java/specs/api/CheckCacheStatus.java
@@ -30,7 +30,7 @@ public class CheckCacheStatus {
         startTime = System.currentTimeMillis();
     }
 
-    @Test(threadPoolSize = 1, invocationCount = 40, timeOut = 3000)
+    @Test(threadPoolSize = 1, invocationCount = 100, timeOut = 3000)
     public void checkCacheStatus() throws InterruptedException {
         Long id = Thread.currentThread().getId();
         //System.out.println("Test method executing on thread with id: " + id);
