c0fede808f1c35ed17afdcaf16a91161869ba0bb
philip_sushkov <pylypsushkov@gmail.com>
2017-10-23 10:24:00 -0400

    few changes - 102317-2

--- src/test/java/specs/PublicSite/CheckPGEcorp.java
+++ src/test/java/specs/PublicSite/CheckPGEcorp.java
@@ -2,6 +2,7 @@ package specs.PublicSite;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import specs.AbstractSpec;
@@ -15,10 +16,11 @@ public class CheckPGEcorp extends AbstractSpec {
         Thread.sleep(DEFAULT_PAUSE);
     }
 
-    @Test
+    @Test (threadPoolSize = 1, invocationCount = 10, timeOut = 1800000)
     public void checkStockPriceUpdates() throws InterruptedException {
         //Assert.assertEquals(homePage.getVersionNumber(), Q4WebVersionNumber, "Displayed version number is incorrect");
 
+        // Original data
         String StockPriceOriginal = driver.findElement(By.xpath("//a[contains(@class, 'StockPrice')]")).getText();
         String StockDateOriginal = driver.findElement(By.xpath("//span[(@class='StockDate')]")).getText();
 
@@ -27,6 +29,27 @@ public class CheckPGEcorp extends AbstractSpec {
         System.out.println("Stock Price Original: " + StockPriceOriginal);
         System.out.println("Stock Date Original: " + StockDateOriginal);
 
+
+        // Updated data
+        String StockPriceUpdated = driver.findElement(By.xpath("//a[contains(@class, 'StockPrice')]")).getText();
+        String StockDateUpdated = driver.findElement(By.xpath("//span[(@class='StockDate')]")).getText();
+
+        Thread.sleep(DEFAULT_PAUSE);
+
+        System.out.println("Stock Price Updated: " + StockPriceUpdated);
+        System.out.println("Stock Date Updated: " + StockDateUpdated);
+
+
+        Assert.assertNotEquals(StockPriceOriginal, StockPriceUpdated, "Stock Price value shouldn't be equal");
+        Assert.assertNotEquals(StockDateOriginal, StockDateUpdated, "Stock Date value shouldn't be equal");
+
+
     }
+
+
+    @AfterTest
+    public void tearDown() {
+    }
+
 
 }
