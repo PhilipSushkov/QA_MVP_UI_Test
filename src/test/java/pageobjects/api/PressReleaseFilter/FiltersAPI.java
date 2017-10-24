eccb6d9061fcb52d0cf3eae3ffb22226f6a4f9cb
V24LAM <v24lam@edu.uwaterloo.ca>
2017-10-24 13:18:39 -0400

    press release API
    pr api

---
+++ src/test/java/pageobjects/api/PressReleaseFilter/FiltersAPI.java
@@ -0,0 +1,50 @@
+package pageobjects.api.PressReleaseFilter;
+
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.http.HttpResponse;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.*;
+
+
+public class FiltersAPI {
+    public static boolean addFilter () throws Exception {
+        try {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader("/Users/victorl/Documents/GitHub/QA-WebCMS-Test/src/test/java/pageobjects/api/PressReleaseFilter/Json/body.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            HttpResponse<JsonNode> response = Unirest.post("https://flfa9qo2eb.execute-api.us-east-1.amazonaws.com/dev/v1/filters")
+                    .header("x-api-key", "w90bwvYsrg8eEtTbFsPsManZM8aOWLPGKjGhZ0Ue")
+                    .header("x-q4org-id", "ea98f2a4d7c94bf294bb929d67c266e5")
+                    .header("content-type", "application/json")
+                    .body(jsonObject)
+                    .asJson();
+            System.out.println(response.getBody());
+            return true;
+        }
+        catch (UnirestException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public static boolean getAllFilters () throws Exception {
+        try {
+            HttpResponse<JsonNode> response = Unirest.get("https://flfa9qo2eb.execute-api.us-east-1.amazonaws.com/dev/v1/filters")
+                    .header("x-api-key", "w90bwvYsrg8eEtTbFsPsManZM8aOWLPGKjGhZ0Ue")
+                    .header("x-q4org-id", "ea98f2a4d7c94bf294bb929d67c266e5")
+                    .asJson();
+            System.out.println(response.getBody());
+            return true;
+        }
+        catch (UnirestException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+}
---
+++ src/test/java/specs/api/PressReleaseFilter/CheckPressReleaseFilter.java
@@ -0,0 +1,34 @@
+package specs.api.PressReleaseFilter;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pageobjects.api.PressReleaseFilter.FiltersAPI;
+import specs.ApiAbstractSpec;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class CheckPressReleaseFilter extends ApiAbstractSpec {
+    @Test
+    public void testFilterApi() {
+        try {
+            FiltersAPI.addFilter();
+            FiltersAPI.getAllFilters();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
