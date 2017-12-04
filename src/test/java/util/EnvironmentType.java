5643781cd8240ee4195ad45e89ce2ff4ed7cb12d
philip_sushkov <pylypsushkov@gmail.com>
2017-12-04 15:01:36 -0500

    Changes - 120417 - 2

--- src/test/java/util/EnvironmentType.java
+++ src/test/java/util/EnvironmentType.java
@@ -11,8 +11,8 @@ public enum EnvironmentType {
   //DEVELOP("enscoreverseproxy.s1.q4web.dev/admin/"),
   //BETA("chicagotest.s1.q4web.newtest/admin/"), //chicagotest url
   BETA("ensco.s1.q4web.newtest/admin"), //facebookrelease url
-  //PRODUCTION("chicagotest.s3.q4preview.com/admin/");
-  PRODUCTION("chicagotest.s3.q4web.com/admin/");
+  PRODUCTION("chicagotest.s3.q4preview.com/admin/");
+  //PRODUCTION("chicagotest.s3.q4web.com/admin/");
   // DEVELOP("admin-dev.q4inc.com/");
 
   private final String host;
