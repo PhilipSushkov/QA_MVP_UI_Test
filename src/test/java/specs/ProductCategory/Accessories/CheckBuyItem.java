8366c99168c93f014aa5c2eab111d5a550cb141f
Pylyp-Sushkov <Pylyp.Sushkov@coxautoinc.com>
2018-08-14 21:18:47 -0400

    The first commit - 081418 - 6

--- src/test/java/specs/ProductCategory/Accessories/CheckBuyItem.java
+++ src/test/java/specs/ProductCategory/Accessories/CheckBuyItem.java
@@ -9,14 +9,18 @@ import specs.AbstractSpec;
 
 public class CheckBuyItem extends AbstractSpec {
     private static BuyItem buyItem;
-    private static ExtentReports buyItemReport;
+    private static ExtentReports buyItemRep;
     private static String sPathToFile, sDataFileJson;
 
-    private final String BUY_ITEM_DATA = "addCustomerData", ITEM_NAME = "addCustomer";
+    private final String BUY_ITEM_DATA = "buyItemData", ITEM_NAME = "buyItem";
 
     @BeforeTest
     public void setUp() {
         buyItem = new BuyItem(driver);
+
+        sPathToFile = System.getProperty("user.dir") + propUIProdCategory.getProperty("dataPath_BuyItem");
+        sDataFileJson = propUIProdCategory.getProperty("json_BuyItemData");
+        buyItemRep = RepBuyItem.GetExtent();
     }
 
     @BeforeMethod
@@ -26,6 +30,7 @@ public class CheckBuyItem extends AbstractSpec {
     // Check Buy Item functionality, positive and negative test cases
     @Test(dataProvider=BUY_ITEM_DATA, priority=1)
     public void checkBuyItem(JSONObject data) throws InterruptedException {
+
 
 
     }
@@ -42,7 +47,7 @@ public class CheckBuyItem extends AbstractSpec {
 
     @AfterTest
     public void tearDown(){
-        buyItemReport.flush();
+        buyItemRep.flush();
     }
 
 }
