8b7e7f53f7a732108d68812683bb31c1047e05a9
PSUSHKOV <pylyp.sushkov@coxautoinc.com>
2019-10-22 16:59:27 -0400

    Few changes - 102219 - 7

--- src/test/java/pageobjects/MyAccount/MyAccount.java
+++ src/test/java/pageobjects/MyAccount/MyAccount.java
@@ -1,9 +1,31 @@
 package pageobjects.MyAccount;
 
+import org.openqa.selenium.WebDriver;
+import pageobjects.AbstractPageObject;
+import util.Log;
+
 /**
  * Created by PSUSHKOV on Oct, 2019
  **/
 
-public class MyAccount {
+public class MyAccount extends AbstractPageObject {
+
+    public MyAccount(WebDriver driver) {
+        super(driver);
+
+        //productCategoryHref = By.xpath(propUIProdCategory.getProperty("href_ProductCategory"));
+    }
+
+    public String getErrorMessage() {
+        try {
+            //waitForElement(transResultsTbl);
+            return driver.getTitle();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.error("Get Login Error message: ElementNotFoundException occurred");
+        }
+
+        return null;
+    }
 
 }
--- src/test/java/specs/MyAccount/CheckMyAccount.java
+++ src/test/java/specs/MyAccount/CheckMyAccount.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.*;
 import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import pageobjects.MyAccount.MyAccount;
 import specs.AbstractSpec;
 import util.Log;
 import java.lang.reflect.Method;
@@ -14,6 +15,7 @@ import java.lang.reflect.Method;
  **/
 
 public class CheckMyAccount extends AbstractSpec {
+    private static MyAccount myAccount;
     private static ExtentReports myAccountRep;
     private static String sPathToFile, sDataFileJson;
     private static final long DEFAULT_PAUSE = 3000;
@@ -22,6 +24,8 @@ public class CheckMyAccount extends AbstractSpec {
 
     @BeforeTest
     public void setUp() {
+        myAccount = new MyAccount(driver);
+
         sPathToFile = System.getProperty("user.dir") + propUIMayAccount.getProperty("dataPath_MyAccount");
         sDataFileJson = propUIMayAccount.getProperty("json_MyAccountData");
         myAccountRep = RepMyAccount.GetExtent();
@@ -39,6 +43,31 @@ public class CheckMyAccount extends AbstractSpec {
 
         // Create the report
         ExtentTest test = myAccountRep.createTest(data.get("name").toString() +"<br>" + data.get("description").toString());
+
+
+        // Split test cases for negative and positive
+        switch (data.get("test_type").toString()) {
+            case "positive":
+                Log.info(method.getName() + " positive test has been finished");
+                break;
+
+            case "negative":
+                String actErrorMessage = myAccount.getErrorMessage();
+                String expErrorMessage = data.get("expErrorMessage").toString();
+
+                if (actErrorMessage.equals(expErrorMessage)) {
+                    test.log(Status.PASS, "Actual Login Error message equals expected one: <b>" + actErrorMessage + "</b>");
+                } else {
+                    test.log(Status.FAIL, "Actual Login Error message doesn't equal expected one: <b>" + actErrorMessage + "</b>. " +
+                            "Supposed to be: <b>" + expErrorMessage + "</b>");
+                }
+
+                Assert.assertTrue(actErrorMessage.equals(expErrorMessage),
+                        "Actual Login Error message: " + actErrorMessage + " doesn't match expected one: " + expErrorMessage);
+
+                Log.info(method.getName() + " negative test has been finished");
+                break;
+        }
 
     }
 
