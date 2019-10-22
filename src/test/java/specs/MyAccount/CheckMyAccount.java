657d94e308cb04e29e64ce9378f04e366a476712
PSUSHKOV <pylyp.sushkov@coxautoinc.com>
2019-10-22 16:25:56 -0400

    Few changes - 102219 - 5

--- src/test/java/specs/MyAccount/CheckMyAccount.java
+++ src/test/java/specs/MyAccount/CheckMyAccount.java
@@ -4,12 +4,10 @@ import com.aventstack.extentreports.*;
 import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.*;
-import pageobjects.ProductCategory.Accessories.*;
 import specs.AbstractSpec;
 import util.Log;
-
 import java.lang.reflect.Method;
-import java.util.HashMap;
+
 
 /**
  * Created by PSUSHKOV on Oct, 2019
@@ -20,5 +18,33 @@ public class CheckMyAccount extends AbstractSpec {
     private static String sPathToFile, sDataFileJson;
     private static final long DEFAULT_PAUSE = 3000;
     private final String DATA = "myAccountData", NAME = "myAccount";
+
+
+    @BeforeTest
+    public void setUp() {
+        sPathToFile = System.getProperty("user.dir") + propUIProdCategory.getProperty("dataPath_BuyItem");
+        sDataFileJson = propUIProdCategory.getProperty("json_BuyItemData");
+        myAccountRep = RepMyAccount.GetExtent();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() throws InterruptedException {
+
+    }
+
+    @DataProvider(name = DATA)
+    public Object[][] buyItemData() {
+        String sPathToDataFile = sPathToFile + sDataFileJson;
+        return genericProvider(NAME, sPathToDataFile);
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+    }
+
+    @AfterTest
+    public void tearDown(){
+        myAccountRep.flush();
+    }
 
 }
---
+++ src/test/java/specs/MyAccount/RepMyAccount.java
@@ -0,0 +1,42 @@
+package specs.MyAccount;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import specs.AbstractSpec;
+
+/**
+ * Created by PSUSHKOV on Oct, 2019
+ **/
+public class RepMyAccount {
+    private static ExtentReports extent;
+    private static ExtentTest test;
+    private static ExtentHtmlReporter htmlReporter;
+    private static String filePath = System.getProperty("user.dir") + AbstractSpec.propUIMayAccount.getProperty("repPath_MyAccount") +
+            "./MyAccountRep.html";
+
+    public static ExtentReports GetExtent() {
+        if (extent != null)
+            return extent; //avoid creating new instance of html file
+        extent = new ExtentReports();
+        extent.attachReporter(getHtmlReporter());
+        return extent;
+    }
+
+    private static ExtentHtmlReporter getHtmlReporter() {
+
+        htmlReporter = new ExtentHtmlReporter(filePath);
+
+        // make the charts visible on report open
+        htmlReporter.config().setChartVisibilityOnOpen(true);
+
+        htmlReporter.config().setDocumentTitle("Check My Account");
+        htmlReporter.config().setReportName("Check My Account, positive and negative test cases");
+        return htmlReporter;
+    }
+
+    public static ExtentTest createTest(String name, String description) {
+        test = extent.createTest(name, description);
+        return test;
+    }
+}
